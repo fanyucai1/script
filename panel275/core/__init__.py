9ec2f29015300b9c9daee22b045daeb6d2af7f6a
fanyucai1 <fanyucai1@126.com>
2019-05-30 15:44:48 +0800

    no

--- panel275/core/__init__.py
+++ panel275/core/__init__.py
@@ -1,5 +1,6 @@
 from . import print_config
 from . import annovar275
-from . import filter_annovar275
+from . import filter_germline
+from . import filter_somatic
 from . import split
 from . import MSI
