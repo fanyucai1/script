b8bb53e504c246a7f72f75dd7e5d1c027301b890
fanyucai1 <fanyucai1@126.com>
2019-05-30 14:04:31 +0800

    no

--- panel275/core/annovar275.py
+++ panel275/core/annovar275.py
@@ -63,7 +63,8 @@ def anno(vcf,out):
                     outfile.write("%s" % (array[dict[out_name[l]]]))
                 else:
                     outfile.write("\t%s" % (array[dict[out_name[l]]]))
-            outfile.write("\t%s\t%s\t%s\n" % (UMT[0], VMT[0], float(VMF[0]) * 100))
+            num=float(VMF[0])*100
+            outfile.write("\t%s\t%s\t%s\n" % (UMT[0], VMT[0], num))
     infile.close()
     outfile.close()
     subprocess.check_call("rm -rf %s.hg19_multianno.txt" %(out),shell=True)
