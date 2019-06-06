c3e33e69bb0538408142f2cec299c52772db5c3e
fanyucai1 <fanyucai1@126.com>
2019-06-06 18:29:24 +0800

    no

--- germline_somatic.py
+++ germline_somatic.py
@@ -24,7 +24,7 @@ def run_split(ivcf,out):
                     pass
     infile.close()
     ###################################################read clinvar vcf to get relationship between position information and OriginSimple
-    infile2=open(clinvar,"r")
+    infile2=open(clinvar,"r",encoding='gb18030')
     dict={}
     for line in infile2:
         line=line.strip()
