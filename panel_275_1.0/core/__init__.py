6907fce3e6481f165d88a32524e06e5a943419a4
fanyucai1 <fanyucai1@126.com>
2019-06-10 14:54:39 +0800

    no

--- panel_275_1.0/core/__init__.py
+++ panel_275_1.0/core/__init__.py
@@ -1,5 +1,6 @@
 from . import print_config
 from . import annovar275
+from . import germline_somatic
 from . import filter_germline
 from . import filter_somatic
 from . import prefilter
--- panel_275_1.0/main_prcoess.py
+++ panel_275_1.0/main_prcoess.py
@@ -15,7 +15,8 @@ parser.add_argument("-p1","--pe1",help="tumor 5 reads",required=True)
 parser.add_argument("-p2","--pe2",help="tumor 3 reads",required=True)
 parser.add_argument("-p","--prefix",help="prefix output",required=True)
 parser.add_argument("-o","--outdir",help="output directory",required=True)
-parser.add_argument("-v","--VAF",help="VAF threshold",choices=[0.02,0.005],required=True,type=float)
+parser.add_argument("-g","--genelist",help="gene list",required=True)
+parser.add_argument("-v","--vaf",help="VAF threshold",choices=[0.02,0.005],required=True,type=float)
 args=parser.parse_args()
 start=time.time()
 if not os.path.exists(args.outdir):
@@ -24,14 +25,16 @@ out=args.outdir+"/"+args.prefix
 a=args.pe1
 b=args.pe2
 purity=0
-if args.VAF==0.02:
+if args.vaf==0.02:
     purity=0.1
 else:
     purity=0.01
 #####################################################################run docker
 core.print_config.tumor_only(a, b, args.prefix, args.outdir,purity)
 #####################################################################filter VAF and genelist
-core.
+core.prefilter.run("%s.smCounter.anno.vcf"%(out),args.genelist,args.vaf,args.outdir,args.prefix)
+#####################################################################split germline and somatic
+core.germline_somatic("%s.vaf.%s.vcf" % (out,args.vaf),"")
 
 
 
