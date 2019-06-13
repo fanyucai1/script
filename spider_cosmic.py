7243c948fcc9e26d7e341aafd05ff63fe43b7894
fanyucai1 <fanyucai1@126.com>
2019-06-13 13:18:19 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import time
 vcf="/data/Database/COSMIC/release_v88/CosmicCodingMuts.hg19.vcf"
 infile=open(vcf,"r")
 outfile=open("cosmic.site.classify.tsv","w")
@@ -12,11 +11,10 @@ for line in infile:
         pattern=re.compile(r'(\d+)')
         id=pattern.findall(array[2])
         url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
-        print(url)
+        print("#%s"%(url))
         res=requests.get(url)
         ret=res.text
         soup=BeautifulSoup(ret,'html.parser')
-        time.sleep(3)
         dt=soup.find_all('dt')
         dd=soup.find_all('dd')
         dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
@@ -24,9 +22,7 @@ for line in infile:
             for i in range(len(dt)):
                 if dt[i].string=="Ever confirmed somatic?":
                     print("%s\t%s"%(array[2],dd[i].string))
-                    outfile.write("%s\t%s\n"%(array[2],dd[i].string))
         else:
             print("%s\tSNP" % (array[2]))
-            outfile.write("%s\tSNP\n" % (array[2]))
 infile.close()
 outfile.close()
