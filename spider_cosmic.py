b92c1e1022c82801fb0543c110fabdf9de7c0583
fanyucai1 <fanyucai1@126.com>
2019-06-13 12:51:44 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -5,41 +5,30 @@ import time
 import os
 
 vcf="/data/Database/COSMIC/release_v88/CosmicCodingMuts.hg19.vcf"
-dict={}
-if os.path.exists("cosmic.site.classify.tsv") and os.path.getsize("cosmic.site.classify.tsv")>0:
-    infile=open("cosmic.site.classify.tsv","r")
-    for line in infile:
-        line=line.strip()
-        array=line.split("\t")
-        dict[array[0]]=1
-    infile.close()
 infile=open(vcf,"r")
 outfile=open("cosmic.site.classify.tsv","w")
 for line in infile:
     line=line.strip()
     if not line.startswith("#"):
         array=line.split("\t")
-        if array[2] in dict:
-            continue
+        pattern=re.compile(r'(\d+)')
+        id=pattern.findall(array[2])
+        url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
+        print (url)
+        res=requests.get(url)
+        ret=res.text
+        soup=BeautifulSoup(ret,'html.parser')
+        dt=soup.find_all('dt')
+        dd=soup.find_all('dd')
+        dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
+        if not dbsnp:
+            for i in range(len(dt)):
+                if dt[i].string=="Ever confirmed somatic?":
+                    print("%s\t%s\n"%(array[2],dd[i].string))
+                    outfile.write("%s\t%s\n"%(array[2],dd[i].string))
         else:
-            pattern=re.compile(r'(\d+)')
-            id=pattern.findall(array[2])
-            url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
-            print (url)
-            res=requests.get(url)
-            ret=res.text
-            soup=BeautifulSoup(ret,'html.parser')
-            dt=soup.find_all('dt')
-            dd=soup.find_all('dd')
-            dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
-            if not dbsnp:
-                for i in range(len(dt)):
-                    if dt[i].string=="Ever confirmed somatic?":
-                        print("%s\t%s\n"%(array[2],dd[i].string))
-                        outfile.write("%s\t%s\n"%(array[2],dd[i].string))
-            else:
-                print("%s\tSNP\n" % (array[2]))
-                outfile.write("%s\tSNP\n" % (array[2]))
-            time.sleep(2)
+            print("%s\tSNP\n" % (array[2]))
+            outfile.write("%s\tSNP\n" % (array[2]))
+        time.sleep(2)
 infile.close()
 outfile.close()
