f960db8b3081fd1ab9c6543e9067248d451c1b39
fanyucai1 <fanyucai1@126.com>
2019-06-13 15:44:24 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -10,20 +10,18 @@ for line in infile:
         pattern=re.compile(r'(\d+)')
         id=pattern.findall(array[2])
         url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
-        res=requests.get(url)
-        print("#%s" % (url))
+        res=requests.get(url,time=10)
         ret = res.text
-        if res.status_code==200:
-            soup=BeautifulSoup(ret,'html.parser')
-            dt=soup.find_all('dt')
-            dd=soup.find_all('dd')
-            dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
-            if dt!=[]:
-                for i in range(len(dt)):
-                    if dt[i].string=="Ever confirmed somatic?":
-                        print("%s\t%s"%(array[2],dd[i].string))
-                        continue
-            else:
-                print("%s\tSNP" % (array[2]))
-                continue
+        soup=BeautifulSoup(ret,'html.parser')
+        dt=soup.find_all('dt')
+        dd=soup.find_all('dd')
+        dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
+        if dt!=[]:
+            for i in range(len(dt)):
+                if dt[i].string=="Ever confirmed somatic?":
+                    print("%s\t%s"%(array[2],dd[i].string))
+                    continue
+        else:
+            print("%s\tSNP" % (array[2]))
+            continue
 infile.close()
