41f902d04dc7de0ecb94af7006f868ffa8540947
fanyucai1 <fanyucai1@126.com>
2019-06-13 21:59:23 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
+import random
+##########在请求头中把User-Agent设置成浏览器中的User-Agent，来伪造浏览器访问
+user_agents = ['Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1','Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50','Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11']
+
 vcf="/data/Database/COSMIC/release_v88/CosmicCodingMuts.hg19.vcf"
 infile=open(vcf,"r")
 outfile=open("cosmic.tsv","w")
@@ -8,12 +13,14 @@ outfile.write("#ID\tType\n")
 dict={}
 for line in infile:
     line=line.strip()
+    time.sleep(random.randint())# 暂停0~1秒，时间区间：[0,1)
     if not line.startswith("#"):
         array=line.split("\t")
         pattern=re.compile(r'(\d+)')
         id=pattern.findall(array[2])
         url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
-        res=requests.get(url)
+        headers = {'User-Agent': random.choice(user_agents)}#随机选择一个User-Agent的
+        res=requests.get(url,headers = headers)
         print(url)
         ret = res.text
         soup=BeautifulSoup(ret,'html.parser')
