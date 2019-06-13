0327a2ec5109ad4798895c73897ec8a56c9377cc
fanyucai1 <fanyucai1@126.com>
2019-06-13 11:38:50 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -17,23 +17,24 @@ infile=open(vcf,"r")
 outfile=open("cosmic.site.classify.tsv","a")
 for line in infile:
     line=line.strip()
-    array=line.split("\t")
-    if array[2] in dict:
-        continue
-    else:
-        url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(array[2])
-        res=requests.get(url)
-        ret=res.text
-        soup=BeautifulSoup(ret,'html.parser')
-        dt=soup.find_all('dt')
-        dd=soup.find_all('dd')
-        dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
-        if not dbsnp:
-            for i in range(len(dt)):
-                if dt[i].string=="Ever confirmed somatic?":
-                    outfile.write("%s\t%s\n"%(array[2],dd[i].string))
+    if not line.startswith("#"):
+        array=line.split("\t")
+        if array[2] in dict:
+            continue
         else:
-            outfile.write("%s\tSNP\n" % (array[2]))
-        time.sleep(2)
+            url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(array[2])
+            res=requests.get(url)
+            ret=res.text
+            soup=BeautifulSoup(ret,'html.parser')
+            dt=soup.find_all('dt')
+            dd=soup.find_all('dd')
+            dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
+            if not dbsnp:
+                for i in range(len(dt)):
+                    if dt[i].string=="Ever confirmed somatic?":
+                        outfile.write("%s\t%s\n"%(array[2],dd[i].string))
+            else:
+                outfile.write("%s\tSNP\n" % (array[2]))
+            time.sleep(2)
 infile.close()
 outfile.close()
