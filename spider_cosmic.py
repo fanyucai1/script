c22d6e15cdd5646783bed6446b9e324294da1813
fanyucai1 <fanyucai1@126.com>
2019-06-13 13:23:54 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -13,16 +13,17 @@ for line in infile:
         url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
         print("#%s"%(url))
         res=requests.get(url)
-        ret=res.text
-        soup=BeautifulSoup(ret,'html.parser')
-        dt=soup.find_all('dt')
-        dd=soup.find_all('dd')
-        dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
-        if not dbsnp:
-            for i in range(len(dt)):
-                if dt[i].string=="Ever confirmed somatic?":
-                    print("%s\t%s"%(array[2],dd[i].string))
-        else:
-            print("%s\tSNP" % (array[2]))
+        if res.status_code==200:
+            ret=res.text
+            soup=BeautifulSoup(ret,'html.parser')
+            dt=soup.find_all('dt')
+            dd=soup.find_all('dd')
+            dbsnp = soup.find(text=re.compile(r'has been flagged as a SNP'))
+            if not dbsnp:
+                for i in range(len(dt)):
+                    if dt[i].string=="Ever confirmed somatic?":
+                        print("%s\t%s"%(array[2],dd[i].string))
+            else:
+                print("%s\tSNP" % (array[2]))
 infile.close()
 outfile.close()
