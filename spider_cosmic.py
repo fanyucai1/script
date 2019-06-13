9c2b3201e331f9203247bff1b98ad773a4e1b9c9
fanyucai1 <fanyucai1@126.com>
2019-06-13 21:34:49 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -20,6 +20,7 @@ for line in infile:
         dt=soup.find_all('dt')
         dd=soup.find_all('dd')
         dbsnp = soup.find(text='The mutation %s has been flagged as a SNP'%(array[2]))
+        print (dbsnp)
         if dbsnp =="%s has been flagged as a SNP" %(array[2]):
             dict[array[2]] = "SNP"
             print("%s\tSNP" % (array[2]))
