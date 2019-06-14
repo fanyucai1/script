082489a032095cee9bc0d75f92cd41dc62ecaddb
fanyucai1 <fanyucai1@126.com>
2019-06-14 09:55:41 +0800

    no

--- general/annovar.py
+++ general/annovar.py
@@ -1,6 +1,8 @@
 import subprocess
 import re
 import sys
+import os
+
 
 annovar="/software/docker_tumor_base/Resource/Annovar/"
 snpsift="/software/SnpEff/4.3/snpEff/"
@@ -9,16 +11,19 @@ java="/software/java/jdk1.8.0_202/bin/java"
 database = ['1000g2015aug_all','1000g2015aug_eas', 'ExAC_ALL', 'esp6500siv2_all','ExAC_EAS','genome_AF','genome_AF_eas','exome_AF','exome_AF_eas']
 out_name=['Chr','Start','End','Ref','Alt','Func.refGene','Gene.refGene','GeneDetail.refGene','ExonicFunc.refGene','AAChange.refGene','cytoBand',
           'avsnp150','snp138','ExAC_ALL','ExAC_EAS','esp6500siv2_all','1000g2015aug_all','1000g2015aug_eas','genome_AF','genome_AF_eas','exome_AF','exome_AF_eas',
-          'cosmic88_coding','CLNALLELEID','CLNDN','CLNDISDB','CLNREVSTAT','CLNSIG','InterVar_automated','SIFT_pred','Canonical_transcript','Polyphen2_HDIV_pred', 'Polyphen2_HVAR_pred','MutationTaster_pred','MutationAssessor_pred','FATHMM_pred',
+          'cosmic88_coding','CLNALLELEID','CLNDN','CLNDISDB','CLNREVSTAT','CLNSIG','InterVar_automated','Canonical_transcript','Total_Depth','Alt_Depth','VAF','GT','SIFT_pred','Polyphen2_HDIV_pred', 'Polyphen2_HVAR_pred','MutationTaster_pred','MutationAssessor_pred','FATHMM_pred',
           'CADD_phred']
 
-def anno(vcf,out):
+def anno(vcf,outdir,prefix):
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    out=outdir+"/"+prefix
     ##########################run snpeff
     cmd = "%s -Xmx40g -jar %s/snpEff.jar -v hg19 -canon -hgvs %s >%s.snpeff.anno.vcf" % (java, snpsift, vcf, out)
     subprocess.check_call(cmd, shell=True)
     ##########################run annovar
-    par=" -protocol refGene,cytoBand,avsnp150,exac03,esp6500siv2_all,1000g2015aug_all,1000g2015aug_eas,gnomad211_exome,gnomad211_genome,cosmic88_coding,clinvar_20190305,ljb26_all,intervar_20180118 "
-    par+=" -operation g,r,f,f,f,f,f,f,f,f,f,f,f "
+    par=" -protocol refGene,cytoBand,snp138,avsnp150,exac03,esp6500siv2_all,1000g2015aug_all,1000g2015aug_eas,gnomad211_exome,gnomad211_genome,cosmic88_coding,clinvar_20190305,ljb26_all,intervar_20180118 "
+    par+=" -operation g,r,f,f,f,f,f,f,f,f,f,f,f,f "
     par+=" -nastring . -polish "
     subprocess.check_call("perl %s/table_annovar.pl %s.snpeff.anno.vcf %s/humandb -buildver hg19 -out %s -remove %s -vcfinput " %(annovar,out,annovar,out,par),shell=True)
     subprocess.check_call("rm -rf %s.hg19_multianno.vcf %s.avinput" %(out,out),shell=True)
@@ -45,16 +50,19 @@ def anno(vcf,out):
             p = re.compile(r'transcript\|(\S+)\|protein_coding')
             a = p.findall(line)
             tmp = array[dict['AAChange.refGene']].split(",")
-            final_nm = tmp[0]
+            Canonical_transcript = tmp[0]
             if a != []:
                 b = a[0].split(".")
                 for j in range(len(tmp)):
                     if re.search(b[0], tmp[j]):
-                        final_nm = tmp[j]
-            array[dict['AAChange.refGene']] = final_nm
+                        Canonical_transcript = tmp[j]
             for l in range(len(out_name)):
                 if l == 0:
                     outfile.write("%s" % (array[dict[out_name[l]]]))
+                elif out_name[l]=="VAF" or out_name[l]=="Total_Depth" or out_name[l]=="Alt_Depth" or out_name[l]=="GT":
+                    outfile.write("\t.")
+                elif out_name[l]=="Canonical_transcript":
+                    outfile.write("\t%s"%(Canonical_transcript))
                 else:
                     outfile.write("\t%s" % (array[dict[out_name[l]]]))
             outfile.write("\n")
@@ -64,10 +72,11 @@ def anno(vcf,out):
     ###########################################################
 
 if __name__=="__main__":
-    if len(sys.argv)!=3:
-        print ("\nUsage:\npython annovar.py vcffile outdir/outprefix\n")
+    if len(sys.argv)!=4:
+        print ("\nUsage:\npython annovar.py vcffile outdir outprefix\n")
         print("Copyright:fanyucai\nVersion:1.0")
         sys.exit(-1)
     vcf=sys.argv[1]
-    out=sys.argv[2]
-    anno(vcf,out)
+    outdir=sys.argv[2]
+    prefix=sys.argv[3]
+    anno(vcf,outdir,prefix)
