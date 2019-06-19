bd960e38c78c3a99d8cd39bda2a3383d8052bc00
fanyucai1 <fanyucai1@126.com>
2019-06-19 14:46:12 +0800

    no

--- spider_cosmic.py
+++ spider_cosmic.py
@@ -5,6 +5,7 @@ import time
 import random
 #############################在请求头中把User-Agent设置成浏览器中的User-Agent，来伪造浏览器访问
 user_agents = ['Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1','Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50','Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11']
+proxy_list =["206.189.228.188:8080","138.197.163.225:8080","38.91.107.213:3128","142.93.124.90:3128"]#设置多个代理服务器
 vcf="CosmicCodingMuts.vcf"
 infile=open(vcf,"r")
 outfile=open("cosmic.tsv","w")
@@ -19,7 +20,11 @@ for line in infile:
         id=pattern.findall(array[2])
         url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
         headers = {'User-Agent': random.choice(user_agents)}#随机选择一个User-Agent的
-        res=requests.get(url,headers=headers,auth=('yucaifan@chosenmedtech.com', 'Fyc_840924'))
+        proxy = random.choice(proxy_list)#随机选取一个代理服务器
+        s = requests.session()
+        s.keep_alive = False
+        s.proxies = {"https":'https://' + proxy}
+        res=s.get(url,headers=headers,auth=('yucaifan@chosenmedtech.com', 'Fyc_840924'))
         print(url)
         ret = res.text
         soup=BeautifulSoup(ret,'html.parser')
