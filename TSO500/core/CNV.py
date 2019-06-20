6c40962076e2f23b7205679e83d1aaee84493281
fanyucai1 <fanyucai1@126.com>
2019-06-20 09:19:20 +0800

    no

---
+++ TSO500/core/CNV.py
@@ -0,0 +1,57 @@
+#Email:fanyucai1@126.com
+
+import os
+import argparse
+import subprocess
+import re
+cmd="cd /software/TSO500/1.3.1/craft_1.0.0.49/resource/ && /software/dotnet/dotnet /software/TSO500/1.3.1/craft_1.0.0.49/Craft.dll " \
+    "-baselineFile craft_baseline.txt -manifestFile craft_manifest.txt  -callGender true -genderThreshold 0.05 " \
+    "-genomeFolder /software/TSO500/1.3.1/resources/genomes/hg19_hardPAR -geneThresholdFile CnvGeneThresholds.csv"
+parser=argparse.ArgumentParser("Run CNV")
+parser.add_argument("-a","--analysis",help="analysis directory",required=True)
+parser.add_argument("-o","--outdir",help="output directory",default=os.getcwd())
+parser.add_argument("-s","--samplelist",help="sample list",required=True)
+args=parser.parse_args()
+
+bam_dir=args.analysis+"/Logs_Intermediates/IndelRealignment/"
+out_dir=args.analysis+"/Logs_Intermediates/CNV/"
+
+
+for(root, dirs, files) in os.walk(bam_dir):
+    sh=""
+    for i in dirs:
+        bam_file=bam_dir+i+"/"+i+".bam"
+        sh=cmd+" -bamFiles "+bam_file+" -outputFolder "+out_dir
+        subprocess.check_call(sh,shell=True)
+infile=open(args.samplelist,"r")
+sample=[]
+for line in infile:
+    line=line.strip()
+    array=re.split('[\t,]',line)
+    sample.append(array[0])
+infile.close()
+for id in sample:
+    path=args.analysis+"/Logs_Intermediates/CNV/%s_CopyNumberVariants.vcf" %(id)
+    if os.path.exists(path):
+        infile=open(path,"r")
+        outfile=open("%s/%s.cnv.tsv" %(args.outdir,id),"w")
+        outfile.write("#Chr\tStart\tend\tRef\tType\tGene\n")
+        i=0
+        print(path)
+        for line in infile:
+            if not line.startswith("#"):
+                line = line.strip()
+                array = line.split()
+                if array[4]=="<DUP>" or array[4]=="<DEL>":
+                    if array[6]=="PASS":
+                        i+=1
+                        p1=re.compile(r'END=(\d+)')
+                        p2=re.compile(r'ANT=(\S+)')
+                        a=p1.findall(line)
+                        b=p2.findall(line)
+                        tmp = array[0] + "\t" + array[1] +"\t"+a[0]+"\t"+array[3]+"\t"+array[4]+"\t"+b[0]
+                        outfile.write("%s\n"%(tmp))
+        outfile.close()
+        if i==0:
+            subprocess.check_call("rm -rf %s/%s.cnv.tsv" %(args.outdir,id),shell=True)
+            print("sample %s not find CNV"%(id)) 
--- spider_cosmic.py
+++ spider_cosmic.py
@@ -1,11 +1,14 @@
 import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+requests.adapters.DEFAULT_RETRIES =10#增加重连次数
 from bs4 import BeautifulSoup
 import re
 import time
 import random
 #############################在请求头中把User-Agent设置成浏览器中的User-Agent，来伪造浏览器访问
 user_agents = ['Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1','Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50','Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11']
-proxy_list =["206.189.228.188:8080","138.197.163.225:8080","38.91.107.213:3128","142.93.124.90:3128"]#设置多个代理服务器
+proxy_list =["158.69.59.125:8888"]#设置多个代理服务器
 vcf="CosmicCodingMuts.vcf"
 infile=open(vcf,"r")
 outfile=open("cosmic.tsv","w")
@@ -13,19 +16,18 @@ outfile.write("#ID\tType\n")
 dict={}
 for line in infile:
     line=line.strip()
-    time.sleep(random.randint(0,2))# 暂停0~1秒，时间区间：[0,1)
+    time.sleep(random.randint(0,1))#暂停0~1秒
     if not line.startswith("#"):
         array=line.split("\t")
         pattern=re.compile(r'(\d+)')
         id=pattern.findall(array[2])
-        url = 'https://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
+        url = 'http://cancer.sanger.ac.uk/cosmic/mutation/overview?genome=37&id=%s' %(id[0])
         headers = {'User-Agent': random.choice(user_agents)}#随机选择一个User-Agent的
         proxy = random.choice(proxy_list)#随机选取一个代理服务器
         s = requests.session()
-        s.keep_alive = False
-        s.proxies = {"https":'https://' + proxy}
-        res=s.get(url,headers=headers,auth=('yucaifan@chosenmedtech.com', 'Fyc_840924'))
-        print(url)
+        s.keep_alive = False#关闭多余连接
+        s.proxies = {"http":'http://' + proxy}
+        res=s.get(url,headers=headers,auth=('yucaifan@chosenmedtech.com', 'Fyc_840924'), verify=False)
         ret = res.text
         soup=BeautifulSoup(ret,'html.parser')
         dbsnp = soup.find_all(text=re.compile("has been flagged as a SNP."))
