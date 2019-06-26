3a5fc43bf9a0fc56268b1f0c5b3ff2c05d5555bc
fanyucai1 <fanyucai1@126.com>
2019-06-26 17:55:57 +0800

    no

--- panel_275_1.0/core/tumor_normal.py
+++ panel_275_1.0/core/tumor_normal.py
@@ -18,10 +18,8 @@ def run(vcf,genelist,vaf,outdir,prefix):
         if not line.startswith("#"):
             array = line.split("\t")
             gene=array[7].split("|")
-            try:
-                print(gene[3])
-            except:
-                print(line)
+            if gene==[]:
+                continue
             if gene[3] in dict:
                 pattern=re.compile(r'Germline_Risk')
                 a=pattern.findall(array[6])
