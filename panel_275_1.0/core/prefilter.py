6aeab352c2fee8b39eded0ace72fe8a503cf90a4
fanyucai1 <fanyucai1@126.com>
2019-06-26 15:42:10 +0800

    no

--- panel_275_1.0/core/prefilter.py
+++ panel_275_1.0/core/prefilter.py
@@ -1,74 +1,49 @@
 #Email:fanyucai1@126.com
 #2019.5.23
 
-import os
 import argparse
 import re
-import sys
 
-def run(vcf,genelist,vaf,outdir,prefix):
-    dict = {}
+def run(genelist,vaf,vcf,outdir,prefix):
+    dict={}
     infile = open(genelist, "r")
     for line in infile:
         line = line.strip()
         dict[line] = 1
     infile.close()
-    infile = open(vcf, "r")
-    all = open("%s/%s.vaf.%s.vcf" % (outdir, prefix,vaf), "w")
+    infile=open(vcf,"r")
+    all = open("%s/%s.vaf.%s.vcf" % (outdir, prefix, vaf), "w")
     all.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
     for line in infile:
-        line = line.strip()
+        line=line.strip()
         if not line.startswith("#"):
-            array = line.split("\t")
-            p1 = re.compile(r'UMT=([0-9,]+)')
-            p2 = re.compile(r'VMT=([0-9,]+)')
-            p3 = re.compile(r'VMF=([0-9,.e-]+)')
-            p4 = re.compile(r',')
-            p5 = re.compile(r'ANN=(\S+)')
-            GT = array[-1].split(":")
-            a = p1.findall(line)  # UMT
-            b = p2.findall(line)  # VMT
-            c = p3.findall(line)  # VMF
-            d = p4.findall(array[4])  # ALT
-            e = p5.findall(line)  # ANNO
-            gene = e[0].split("|")  # gene name
-            if gene != [] and gene[3] in dict:
-                if a == [] and b == [] and c == [] and d == []:
-                    p1 = re.compile(r'DP=([0-9,]+)')
-                    p2 = re.compile(r'VD=([0-9,]+)')
-                    p3 = re.compile(r'AF=([0-9.,e-]+)')
-                    a = p1.findall(line)  # DP
-                    b = p2.findall(line)  # VD
-                    c = p3.findall(line)  # AF
-                if d == []:
-                    tmp = "%s\t%s\t.\t%s\t%s\t.\t.\tUMT=%s;VMT=%s;VMF=%s;GT=%s" % (
-                    array[0], array[1], array[3], array[4], a[0], b[0], c[0], GT[0])
-                    if float(c[0]) >= vaf:
-                        all.write("%s\n" % (tmp))
-                else:
-                    VMT = b[0].split(",")
-                    VMF = c[0].split(",")
-                    ALT = array[4].split(",")
-                    for i in range(len(VMT)):
-                        tmp = "%s\t%s\t.\t%s\t%s\t.\t.\tUMT=%s;VMT=%s;VMF=%s;GT=%s" % (
-                        array[0], array[1], array[3], ALT[i], a[0], VMT[i], VMF[i], GT[0])
-                        if float(VMF[i]) >= vaf:
-                            all.write("%s\n" % (tmp))
+            p = re.compile(r'ANN=(\S+)')
+            gene=p.findall(line)
+            if gene[3] in dict:
+                array=line.split("\t")
+                info=array[-1].split(":")
+                GT=info[0]
+                AD=info[1].split(",")
+                VF=info[2].split(",")
+                Ref_Reads =AD[0]
+                if VF==[]:
+                    Alt_Reads=AD[1]
+                    Var=info[2]
+                    if float(Var) >= vaf:
+                        all.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n"%(array[0],array[1],array[2],array[3],array[4],Ref_Reads,Alt_Reads,GT,Var))
+                    for i in range(1,len(AD)):
+                        Alt_Reads=AD[i]
+                        Var=VF[i-1]
+                        if float(Var)>=vaf:
+                            all.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n" % (array[0], array[1], array[2], array[3], array[4], Ref_Reads, Alt_Reads, GT, Var))
     infile.close()
     all.close()
-
 if __name__=="__main__":
-    if len(sys.argv)!=6:
-        print("Usage:\npython3 %s vcf genelist vaf outdir prefix")
-        print("Email:fanyucai1@126.com")
-        print("Version:1.0")
-        sys.exit(-1)
-    vcf=sys.argv[1]
-    genelist=sys.argv[2]
-    vaf=sys.argv[3]
-    outdir=sys.argv[4]
-    prefix=sys.argv[5]
-    run(vcf,genelist,vaf,outdir,prefix)
-
-
-
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-g","--genelist",help="gene list",required=True)
+    parser.add_argument("-v","--vaf",help="VAF",required=True)
+    parser.add_argument("-v","--vcf",help="vcf",required=True)
+    parser.add_argument("-o","--outdir",help="output directory",required=True)
+    parser.add_argument("-p","--prefix",help="prefix output",required=True)
+    args=parser.parse_args()
+    run(args.genelist,args.vaf,args.vcf,args.outdir,args.prefix)
--- panel_275_1.0/core/tumor_normal.py
+++ panel_275_1.0/core/tumor_normal.py
@@ -1,7 +1,13 @@
 import argparse
 import re
 
-def run(vcf,outdir,prefix):
+def run(genelist,vaf,vcf,outdir,prefix):
+    dict={}
+    infile = open(genelist, "r")
+    for line in infile:
+        line = line.strip()
+        dict[line] = 1
+    infile.close()
     infile=open(vcf,"r")
     somatic=open("%s/%s.somatic.vcf"%(outdir,prefix),"w")
     somatic.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
@@ -10,24 +16,43 @@ def run(vcf,outdir,prefix):
     for line in infile:
         line=line.strip()
         if not line.startswith("#"):
-            array=line.split("\t")
-            pattern=re.compile(r'Germline_Risk')
-            a=pattern.findall(array[6])
-            info=array[-1].split(":")
-            Reads=info[1].split(",")
-            if a!=[]:
-                germline.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n"
-                               %(array[0],array[1],array[2],array[3],array[4],Reads[0],Reads[1],info[0],info[2]))
-            else:
-                somatic.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n"
-                               % (array[0], array[1], array[2], array[3], array[4], Reads[0], Reads[1], info[0], info[2]))
+            p = re.compile(r'ANN=(\S+)')
+            gene=p.findall(line)
+            if gene[3] in dict:
+                array=line.split("\t")
+                pattern=re.compile(r'Germline_Risk')
+                a=pattern.findall(array[6])
+                info=array[-1].split(":")
+                GT=info[0]
+                AD=info[1].split(",")
+                VF=info[2].split(",")
+                Ref_Reads =AD[0]
+                if VF==[]:
+                    Alt_Reads=AD[1]
+                    Var=info[2]
+                    if float(Var) >= vaf:
+                        if a!=[]:#germline
+                            germline.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n"%(array[0],array[1],array[2],array[3],array[4],Ref_Reads,Alt_Reads,GT,Var))
+                        else:
+                            somatic.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n"% (array[0], array[1], array[2], array[3], array[4], Ref_Reads,Alt_Reads,GT,Var))
+                else:
+                    for i in range(1,len(AD)):
+                        Alt_Reads=AD[i]
+                        Var=VF[i-1]
+                        if float(Var)>=vaf:
+                            if a != []:  # germline
+                                germline.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n" % (array[0], array[1], array[2], array[3], array[4], Ref_Reads, Alt_Reads, GT, Var))
+                            else:
+                                somatic.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n" % (array[0], array[1], array[2], array[3], array[4], Ref_Reads, Alt_Reads, GT, Var))
     infile.close()
     somatic.close()
     germline.close()
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
+    parser.add_argument("-g","--genelist",help="gene list",required=True)
+    parser.add_argument("-v","--vaf",help="VAF",required=True)
     parser.add_argument("-v","--vcf",help="vcf",required=True)
     parser.add_argument("-o","--outdir",help="output directory",required=True)
     parser.add_argument("-p","--prefix",help="prefix output",required=True)
     args=parser.parse_args()
-    run(args.vcf,args.outdir,args.prefix)
+    run(args.genelist,args.vaf,args.vcf,args.outdir,args.prefix)
