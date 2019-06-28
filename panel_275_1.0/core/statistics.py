ba73381e6196024eafdc2d5764691e9405239869
fanyucai1 <fanyucai1@126.com>
2019-06-28 13:51:54 +0800

    no

--- panel_275_1.0/core/statistics.py
+++ panel_275_1.0/core/statistics.py
@@ -8,26 +8,20 @@ def run(indir="/data/Panel275/"):
     MSI={}
     sample=[]
     for root,dirs,files in os.walk(indir):
-        filename=[]
         for file in files:
             pattern = re.compile(r'msi.tsv$')
             if pattern.findall(file):
-                filename.append(file)
-                print(os.path.join(root, file))
-            for name in filename:
-                sample=name.split(".")
-                tmp=root+"/"+sample[0]+"/result/MSI/"+name
-                if os.path.exists(tmp):
-                    infile=open(tmp,'r')
-                    num=0
-                    for line in infile:
-                        num+=1
-                        line=line.strip()
-                        array=line.split()
-                        if num==2:
-                            id=name.split(".")
-                            MSI[id[0]]=array[2]
-                            print (id[0],"\t",array[2])
+                filename=os.path.join(root, file)
+                sample=file.split(".")
+                infile=open(filename,'r')
+                num=0
+                for line in infile:
+                    num+=1
+                    line=line.strip()
+                    array=line.split()
+                    if num==2:
+                        MSI[sample[0]]=array[2]
+                        print (sample[0],"\t",array[2])
 
 if __name__=="__main__":
     run(indir="/data/Panel275/")
