6b6ba0e9c6a61196709d9fcafb2cf9ab1cd616da
fanyucai1 <fanyucai1@126.com>
2019-06-28 13:05:10 +0800

    no

--- panel_275_1.0/core/statistics.py
+++ panel_275_1.0/core/statistics.py
@@ -8,10 +8,9 @@ def run(indir="/data/Panel275/"):
     MSI={}
     for root,dirs,files in os.walk(indir):
         for file in files:
+            print (file)
             for dir in dirs:
                 tmp=root+"/"+dir+"/"+file
-                if os.path.exists(tmp):
-                    print(tmp)
                 pattern=re.compile(r'msi.tsv')
                 if pattern.findall(tmp) and os.path.exists(tmp):
                     infile=open(tmp,'r')
