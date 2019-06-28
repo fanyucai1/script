0551b1afd1afd2c2017e5154ec895aa186c654cd
fanyucai1 <fanyucai1@126.com>
2019-06-28 10:19:35 +0800

    no

--- panel_275_1.0/core/print_config.py
+++ panel_275_1.0/core/print_config.py
@@ -68,11 +68,15 @@ def tumor_only(p1,p2,sampelID,outdir,purity,sex,typen,n1="0",n2="0"):
                    % (purity,sampelID, pe1, pe2))
     if type=="cfDNA":
         outfile.write(
-            "refUmiFiles =/srv/qgen/data/base_line_cfDNA/%s1.sum.primer.umis.txt,/srv/qgen/data/base_line_cfDNA/%s2.sum.primer.umis.txt,/srv/qgen/data/base_line_cfDNA/%s3.sum.primer.umis.txt,/srv/qgen/data/base_line_cfDNA/%s4.sum.primer.umis.txt\n"
+            "refUmiFiles =/srv/qgen/data/base_line_cfDNA/%s1.sum.primer.umis.txt,/srv/qgen/data/base_line_cfDNA/%s2.sum.primer.umis.txt,/srv/qgen/data/base_line_cfDNA/%s3.sum.primer.umis.txt,/srv/qgen/data/base_line_cfDNA/%s4.sum.primer.umis.txt"
             % (sex, sex, sex,sex))
     else:
-        outfile.write("refUmiFiles =/srv/qgen/data/base_line_tissue/%s1.sum.primer.umis.txt,/srv/qgen/data/base_line_tissue/%s2.sum.primer.umis.txt,/srv/qgen/data/base_line_tissue/%s3.sum.primer.umis.txt\n"
+        outfile.write("refUmiFiles =/srv/qgen/data/base_line_tissue/%s1.sum.primer.umis.txt,/srv/qgen/data/base_line_tissue/%s2.sum.primer.umis.txt,/srv/qgen/data/base_line_tissue/%s3.sum.primer.umis.txt"
                       %(sex,sex,sex))
+    if n1 != "0" and n2 != "0":
+        outfile.write(",/project/normal.sum.primer.umis.txt\n")
+    else:
+        outfile.write("\n")
     ####################################normal sample
     cmd=""
     if n1 != "0" and n2 != "0":
