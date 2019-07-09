0b8acf58b3e0069e38c31efb95f40142abf878af
fanyucai1 <fanyucai1@126.com>
2019-07-09 15:20:45 +0800

    no

--- samefile.py
+++ samefile.py
@@ -10,21 +10,31 @@ dict={}
 infile1=open(vardict,"r")
 for line in infile1:
     line=line.strip()
-    dict[line]=1
+    array=line.split("\t")
+    if len(array[3])>=2  and len(array[3])==len(array[4]):#MNV
+        tmp1=array[3].split()
+        tmp2=array[4].split()
+        for j in range(len(tmp1)):
+            tmp=array[0]+"_"+array[1]+"_"+array[2]+"_"+tmp1[j]+"_"+tmp2[j]
+            dict[tmp] = line
 infile1.close()
 
 infile2=open(varscan_indel,"r")
 for line in infile2:
     line = line.strip()
-    if line in dict:
-        outfile.write("%s\n"%(line))
+    array = line.split("\t")
+    tmp =array[0]+"_"+array[1]+"_"+array[2]+"_"+array[3]+"_"+array[4]
+    if tmp in dict:
+        outfile.write("%s\n"%(dict[tmp]))
 infile2.close()
 
 infile3=open(varscan_snp,"r")
 for line in infile3:
     line = line.strip()
-    if line in dict:
-        outfile.write("%s\n"%(line))
+    array = line.split("\t")
+    tmp =array[0]+"_"+array[1]+"_"+array[2]+"_"+array[3]+"_"+array[4]
+    if tmp in dict:
+        outfile.write("%s\n"%(dict[tmp]))
 infile3.close()
 
 outfile.close()
