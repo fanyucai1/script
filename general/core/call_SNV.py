5e91a5aa097ba8804dd67601938111fa6c772ebc
fanyucai1 <fanyucai1@126.com>
2019-07-10 12:45:09 +0800

    no

--- general/core/call_SNV.py
+++ general/core/call_SNV.py
@@ -5,12 +5,14 @@ import re
 import argparse
 import os
 from multiprocessing import Process
-varscan="/software/VarScan2/VarScan.v2.4.3.jar"
-samtools="/software/samtools/samtools-1.9/bin/samtools"
-ref="/data/Database/hg19/ucsc.hg19.fasta"
-env = "export PATH=/software/java/jdk1.8.0_202/bin:/software/R/R-v3.5.2/bin/:"
-env += "/software/vardict/1.5.7/VarDictJava-1.5.7/bin:/software/perl/perl-v5.28.1/bin/:"
-env+="/software/samtools/samtools-1.9/bin/:$PATH"
+import configparser
+class Myconf(configparser.ConfigParser):
+    def __init__(self, defaults=None):
+        configparser.ConfigParser.__init__(self, defaults=defaults)
+    def optionxform(self, optionstr):
+        return optionstr
+
+
 def vardict_pair(args):
     vaf, tname, tbam, nbam, bed, nname, outdir=args.vaf,args.tumor,args.tb,args.nb,args.bed,args.normal,args.prefix
     cmd="%s && VarDict -q 20 -Q 10 -G %s -f %s -N %s -b \"%s|%s\" -z -c 1 -S 2 -E 3 -g 4 %s |testsomatic.R |var2vcf_paired.pl -N \"%s|%s\" -f %s >%s/%s.vardict.vcf" \
@@ -44,31 +46,32 @@ def varscan_pair(args):
         subprocess.check_call("%s" % (cmd), shell=True)
     a = "%s && samtools mpileup -f %s %s %s >%s_normal.mpileup" % (env, ref, par, normal, out)
     b = "%s && samtools mpileup -f %s %s %s >%s_tumor.mpileup" % (env, ref, par, tumor, out)
-    if __name__ == '__main__':
-        p1 = Process(target=shell_run, args=(a,))
-        p2 = Process(target=shell_run, args=(b,))
-        p1.start()
-        p2.start()
-        p1.join()
-        p2.join()
-        subprocess.check_call(
-            "%s && cd %s && java -Xmx10g -jar %s somatic %s_normal.mpileup %s_tumor.mpileup %s --strand-filter 1 --output-vcf 1 --min-var-freq %s --min-coverage 50"
-            % (env,outdir, varscan, prefix, prefix, prefix, vaf), shell=True)
-        # filter snp around indel
-        subprocess.check_call(
-            "%s && cd %s && java -Xmx10g -jar %s somaticFilter %s.snp.vcf --min-coverage 50 --indel-file %s.indel.vcf --output-file %s.filter.snp.vcf --min-var-freq %s"
-            % (env,outdir, varscan, prefix, prefix, prefix,vaf), shell=True)
-        # separate a somatic output file by somatic_status (Germline, Somatic, LOH)
-        c = "%s && cd %s && java -Xmx10g -jar %s processSomatic %s.filter.snp.vcf --min-tumor-freq %s" % (env,outdir, varscan, prefix, vaf)
-        d = "%s && cd %s && java -Xmx10g -jar %s processSomatic %s.indel.vcf --min-tumor-freq %s" % (env,outdir, varscan, prefix, vaf)
-        p3 = Process(target=shell_run, args=(c,))
-        p4 = Process(target=shell_run, args=(d,))
-        p3.start()
-        p4.start()
-        p3.join()
-        p4.join()
+    p1 = Process(target=shell_run, args=(a,))
+    p2 = Process(target=shell_run, args=(b,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    subprocess.check_call(
+        "%s && cd %s && java -Xmx10g -jar %s somatic %s_normal.mpileup %s_tumor.mpileup %s --strand-filter 1 --output-vcf 1 --min-var-freq %s --min-coverage 50"
+        % (env,outdir, varscan, prefix, prefix, prefix, vaf), shell=True)
+    # filter snp around indel
+    subprocess.check_call(
+        "%s && cd %s && java -Xmx10g -jar %s somaticFilter %s.snp.vcf --min-coverage 50 --indel-file %s.indel.vcf --output-file %s.filter.snp.vcf --min-var-freq %s"
+        % (env,outdir, varscan, prefix, prefix, prefix,vaf), shell=True)
+    # separate a somatic output file by somatic_status (Germline, Somatic, LOH)
+    c = "%s && cd %s && java -Xmx10g -jar %s processSomatic %s.filter.snp.vcf --min-tumor-freq %s" % (env,outdir, varscan, prefix, vaf)
+    d = "%s && cd %s && java -Xmx10g -jar %s processSomatic %s.indel.vcf --min-tumor-freq %s" % (env,outdir, varscan, prefix, vaf)
+    p3 = Process(target=shell_run, args=(c,))
+    p4 = Process(target=shell_run, args=(d,))
+    p3.start()
+    p4.start()
+    p3.join()
+    p4.join()
 
+##########################################################################
 parser = argparse.ArgumentParser("Call SNV from tumor-normal use vardict and varscan.")
+parser.add_argument("-c","--config",required=True,help="config file")
 subparsers = parser.add_subparsers(dest='SNV')
 parser_a = subparsers.add_parser("vardict",help="vardict call SNV")
 parser_a.add_argument("-b", "--bed", help="target bed file", default="0", type=str)
@@ -89,4 +92,16 @@ parser_b.add_argument("-p", "--prefix", help="prefix of output", required=True)
 parser_b.set_defaults(func=varscan_pair)
 
 args = parser.parse_args()
+##############################################
+config = Myconf()
+config.read(args.config)
+java=config['software']['java']
+R=config['software']['R']
+perl=config['software']['perl']
+VarDict=config['software']['VarDict']
+samtools=config['software']['samtools']
+ref=config['database']['ref']
+varscan=config['software']['varscan']
+env="export PATH=%s:%s:%s:%s:$PATH" %(java,R,perl,VarDict)
+##############################################
 args.func(args)
