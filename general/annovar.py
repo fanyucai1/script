9e5c60e374b3fccc1722f75633dbc2bb7a8c00d5
fanyucai1 <fanyucai1@126.com>
2019-07-10 20:58:18 +0800

    no

--- general/annovar.py
+++ general/annovar.py
@@ -1,30 +1,41 @@
+#Email:fanyucai1@126.com
+#2019.7.5
+
+import argparse
+import os
 import subprocess
 import re
-import sys
-import os
-
-
+Canonical_transcript_file="/data/Database/knownCanonical/clinvar_canonical_trans.txt"
+out_name=['Chr','Start','End','Ref','Alt','Func.refGene','Gene.refGene','GeneDetail.refGene',
+          'ExonicFunc.refGene',	'AAChange.refGene',	'cytoBand',	'1000g2015aug_all',	'avsnp150',	'snp138',
+          'CLNALLELEID','CLNDN','CLNDISDB',	'CLNREVSTAT','CLNSIG','cosmic88_coding','SIFT_score','SIFT_pred',
+          'Polyphen2_HDIV_score','Polyphen2_HDIV_pred','esp6500siv2_all','ExAC_ALL','ExAC_EAS','1000g2015aug_eas',
+          '1000g2015aug_sas','1000g2015aug_afr','1000g2015aug_amr','1000g2015aug_eur','InterVar_automated','GT','AAChange.1',
+          'Ref_Reads',	'Alt_Reads','Var']
 annovar="/software/docker_tumor_base/Resource/Annovar/"
-snpsift="/software/SnpEff/4.3/snpEff/"
-java="/software/java/jdk1.8.0_202/bin/java"
-
-database = ['1000g2015aug_all','1000g2015aug_eas', 'ExAC_ALL', 'esp6500siv2_all','ExAC_EAS','genome_AF','genome_AF_eas','exome_AF','exome_AF_eas']
-out_name=['Chr','Start','End','Ref','Alt','Func.refGene','Gene.refGene','GeneDetail.refGene','ExonicFunc.refGene','AAChange.refGene','cytoBand',
-          'avsnp150','snp138','ExAC_ALL','ExAC_EAS','esp6500siv2_all','1000g2015aug_all','1000g2015aug_eas','genome_AF','genome_AF_eas','exome_AF','exome_AF_eas',
-          'cosmic88_coding','CLNALLELEID','CLNDN','CLNDISDB','CLNREVSTAT','CLNSIG','InterVar_automated','Canonical_transcript','Total_Depth','Alt_Depth','VAF','GT','SIFT_pred','Polyphen2_HDIV_pred', 'Polyphen2_HVAR_pred','MutationTaster_pred','MutationAssessor_pred','FATHMM_pred',
-          'CADD_phred']
-
-def anno(vcf,outdir,prefix):
+def run(vcf,prefix,outdir):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     out=outdir+"/"+prefix
+    #########################get Canonical transcript info
+    transcript={}
+    infile=open(Canonical_transcript_file,"r")
+    for line in infile:
+        line=line.strip()
+        array=line.split("\t")
+        transcript[array[0]]=[]
+        for j in range(len(array)):
+            if j!=0:
+                tmp=array[j].split(".")
+                transcript[array[0]].append(tmp[0])
+    infile.close()
     ##########################run annovar
-    par=" -protocol refGene,cytoBand,snp138,avsnp150,exac03,esp6500siv2_all,1000g2015aug_all,1000g2015aug_eas,gnomad211_exome,gnomad211_genome,cosmic88_coding,clinvar_20190305,ljb26_all,intervar_20180118 "
-    par+=" -operation g,r,f,f,f,f,f,f,f,f,f,f,f,f "
-    par+=" -nastring . -polish "
-    subprocess.check_call("perl %s/table_annovar.pl %s %s/humandb -buildver hg19 -out %s -remove %s -vcfinput " %(annovar,vcf,annovar,out,par),shell=True)
-    subprocess.check_call("rm -rf %s.hg19_multianno.vcf %s.avinput" %(out,out),shell=True)
-    ###########################
+    par = " -protocol refGene,cytoBand,snp138,avsnp150,exac03,esp6500siv2_all,1000g2015aug_all,1000g2015aug_eas,gnomad211_exome,gnomad211_genome,cosmic88_coding,clinvar_20190305,ljb26_all,intervar_20180118"
+    par += ",1000g2015aug_sas,1000g2015aug_afr,1000g2015aug_amr,1000g2015aug_eur "
+    par += " -operation g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f "
+    par += " -nastring . -polish "
+    subprocess.check_call("perl %s/table_annovar.pl %s %s/humandb -buildver hg19 -out %s -remove %s -vcfinput " % (annovar, vcf, annovar, out, par), shell=True)
+    subprocess.check_call("rm -rf %s.hg19_multianno.vcf %s.avinput" % (out, out), shell=True)
     infile = open("%s.hg19_multianno.txt" % (out), "r")
     outfile = open("%s.annovar.tsv" % (out), "w")
     for i in range(len(out_name)):
@@ -43,27 +54,57 @@ def anno(vcf,outdir,prefix):
                 name.append(array[i])
                 dict[array[i]] = i
         else:
+            p1 = re.compile(r'Alt_Reads=([0-9]+)')
+            p2 = re.compile(r'Ref_Reads=([0-9]+)')
+            p3 = re.compile(r'Var=(\d+.\d+)')
+            p4 = re.compile(r'GT=(\d+/\d+)')
+            Alt_Reads = p1.findall(line)
+            Ref_Reads = p2.findall(line)
+            Var = p3.findall(line)
+            GT = p4.findall(line)
+            ##########################format output knownCanonical transcript
+            tmp = array[dict['AAChange.refGene']].split(",")
+            final_nm = ""
+            if array[6] in transcript:
+                for i in transcript[array[6]]:
+                    if final_nm == "":
+                        for k in tmp:
+                            if final_nm=="" and re.search(i,k):
+                                final_nm = k
+                                continue
+                            else:
+                                pass
+                    else:
+                        continue
+            if final_nm=="":
+                final_nm=tmp[0]
             for l in range(len(out_name)):
                 if l == 0:
                     outfile.write("%s" % (array[dict[out_name[l]]]))
-                elif out_name[l]=="VAF" or out_name[l]=="Total_Depth" or out_name[l]=="Alt_Depth" or out_name[l]=="GT":
-                    outfile.write("\t.")
-                elif out_name[l]=="Canonical_transcript":
-                    outfile.write("\t.")
+                elif out_name[l] == "Var":
+                    tmp_num = float(Var[0]) * 100
+                    outfile.write("\t%.2f" % (tmp_num) + "%")
+                elif out_name[l] == "Alt_Reads":
+                    outfile.write("\t%s" % (Alt_Reads[0]))
+                elif out_name[l] == "Ref_Reads":
+                    outfile.write("\t%s" % (Ref_Reads[0]))
+                elif out_name[l] == "AAChange.1":
+                    outfile.write("\t%s" % (final_nm))
+                elif out_name[l] == "GT":
+                    outfile.write("\t%s" % (GT[0]))
                 else:
                     outfile.write("\t%s" % (array[dict[out_name[l]]]))
             outfile.write("\n")
     infile.close()
     outfile.close()
-    subprocess.check_call("rm -rf %s.hg19_multianno.txt %s/snpEff_summary.html %s/snpEff_genes.txt %s.snpeff.anno.vcf" %(out,outdir,outdir,out),shell=True)
+    if os.path.exists("%s.hg19_multianno.txt" % (out)):
+        subprocess.check_call("rm -rf %s.hg19_multianno.txt" % (out), shell=True)
     ###########################################################
 
 if __name__=="__main__":
-    if len(sys.argv)!=4:
-        print ("\nUsage:\npython annovar.py vcffile outdir outprefix\n")
-        print("Copyright:fanyucai\nVersion:1.0")
-        sys.exit(-1)
-    vcf=sys.argv[1]
-    outdir=sys.argv[2]
-    prefix=sys.argv[3]
-    anno(vcf,outdir,prefix)
+    parser=argparse.ArgumentParser("")
+    parser.add_argument("-v","--vcf",help="format vcf file",required=True)
+    parser.add_argument("-p","--prefix",help="prefix of output",required=True)
+    parser.add_argument("-o","--outdir",help="output directory",required=True)
+    args=parser.parse_args()
+    run(args.vcf,args.prefix,args.outdir)
