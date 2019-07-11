015098a4b6661f65401f4bca698b8101099f7154
fanyucai1 <fanyucai1@126.com>
2019-07-11 10:55:54 +0800

    no

--- EMQN/prefilter.py
+++ EMQN/prefilter.py
@@ -8,12 +8,45 @@ def run(anno_vcf,low_vcf,outdir,prefix):
     outfile.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
     for line in infile1:
         line=line.strip()
-        array=line.split("\t")
         if not line.startswith("#"):
-            a=array[4].split(",")
+            array=line.split("\t")
             info=array[-1].split(":")
             GT=info[0]
-            Ref
-            if len(a)==1:
-                outfile.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
-                          %(array[0],array[1],array[2],array[3],array[4],array[0]))
+            AD=info[1].split(",")
+            VF=info[2].split(",")
+            ALT=array[4].split(",")
+            Ref_Reads =AD[0]
+            if len(VF)==1:
+                Alt_Reads=AD[1]
+                Var=VF[0]
+                outfile.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n"%(array[0],array[1],array[2],array[3],array[4],Ref_Reads,Alt_Reads,GT,Var))
+            else:
+                for i in range(1,len(AD)):
+                    Alt_Reads=AD[i]
+                    Var=VF[i-1]
+                    outfile.write("%s\t%s\t%s\t%s\t%s\t.\t.\tRef_Reads=%s;Alt_Reads=%s;GT=%s;Var=%s\n" % (array[0], array[1], array[2], array[3], ALT[i-1], Ref_Reads, Alt_Reads, GT, Var))
+    infile1.close()
+    dict={}
+    for line in infile2:
+        line = line.strip()
+        array=line.split("\t")
+        if line.startswith("READ_SET"):
+            for i in range(len(array)):
+                if array[i] in('CHROM', 'POS','ID','REF','ALT','QUAL','FILTER','UMT','VMT','VMF'):
+                    dict[array[i]]=i
+        else:
+            Total_reads,Ref_Reads,Alt_Reads,Var="","","",""
+            for i in range(len(array)):
+                if dict[array[i]]=="CHROM":
+                    outfile.write("%s"%(array[i]))
+                elif dict[array[i]]=="UMT":
+                    Total_reads=array[i]
+                elif dict[array[i]]=="VMT":
+                    ALT_Reads=array[i]
+                elif dict[array[i]] == "VMF":
+                    Var=array[i]
+                else:
+                    outfile.write("\t%s" % (array[i]))
+                Ref_Reads=int(Total_reads)-int(Alt_Reads)
+                outfile.write("\tRef_Reads=%s;Alt_Reads=%s;GT=.;Var=%s\n"%(Ref_Reads,Alt_Reads,Var))
+            outfile.write()        
