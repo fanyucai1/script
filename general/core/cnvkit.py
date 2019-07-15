a31ede31e680df7eb28c3e93a996c3cf9f29d73f
fanyucai1 <fanyucai1@126.com>
2019-07-15 15:54:59 +0800

    no

--- general/core/cnvkit.py
+++ general/core/cnvkit.py
@@ -3,11 +3,30 @@
 
 import subprocess
 import argparse
-
+import glob
 #http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/refFlat.txt.gz
 def run(tumor,normal,bed,outdir,anno,access,python3,cnvkit,ref):
     cmd="%s %s batch %s --normal %s --targets %s --fasta %s --output-reference my_reference.cnn --output-dir %s --annotate %s --access %s" %(python3,cnvkit,tumor,normal,bed,ref,outdir,anno,access)
     subprocess.check_call(cmd,shell=True)
+    cns=glob.glob("%s/*.cns")
+    infile=open(cns[0],"r")
+    outfile=open("%s/cnv.final.tsv","w")
+    outfile.write("#Chr\tStart\tend\tgene\tlog2\ttype\tCopy\n")
+    for line in infile:
+        if not line.startswith("chromosome"):
+            line=line.strip()
+            array=line.split("\t")
+            type=""
+            copy = 2 ** float(array[4]) * 2
+            if float(array[4])>=0.585:
+                type="gain"
+                tmp = array[0] + "\t" + array[1] + "\t" + array[2] + "\t" + array[3] + "\t" + array[4] + "\t" + type + "\t" + str(copy)
+                outfile.write("%s\n" % (tmp))
+            if float(array[4]) <=-1:
+                type="loss"
+                tmp=array[0]+"\t"+array[1]+"\t"+array[2]+"\t"+array[3]+"\t"+array[4]+"\t"+type+"\t"+str(copy)
+                outfile.write("%s\n"%(tmp))
+    outfile.close()
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser("cnvkit run cnv.")
