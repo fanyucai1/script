03faf0bc6d8f7599d00953e42e6164033052172b
fanyucai1 <fanyucai1@126.com>
2019-07-16 12:03:15 +0800

    no

--- vardict/vardict_pair.py
+++ vardict/vardict_pair.py
@@ -4,12 +4,12 @@ env="export PATH=/software/java/jdk1.8.0_202/bin:/software/R/R-v3.5.2/bin/:"
 env+="/software/vardict/1.5.7/VarDictJava-1.5.7/bin:/software/perl/perl-v5.28.1/bin/:$PATH"
 
 def tumor_normal(args):
-    cmd="%s && VarDict -q 20 -Q 20 -G %s -f %s -N %s -r 5 -b \"%s|%s\" -z -c 1 -S 2 -E 3 -g 4 %s |testsomatic.R |var2vcf_paired.pl -M -N \"%s|%s\" -f %s >%s/%s.vardict.vcf" \
-        %(env,args.ref,args.vaf,args.tn,args.tb,args.nb,args.bed,args.tm,args.nn,args.vaf,args.outdir,args.tn)
+    cmd="%s && VarDict -q 20 -Q 20 -G %s -f %s -N %s -r %s -b \"%s|%s\" -z -c 1 -S 2 -E 3 -g 4 %s |testsomatic.R |var2vcf_paired.pl -M -N \"%s|%s\" -f %s >%s/%s.vardict.vcf" \
+        %(env,args.ref,args.vaf,args.tn,args.min,args.tb,args.nb,args.bed,args.tm,args.nn,args.vaf,args.outdir,args.tn)
     subprocess.check_call(cmd,shell=True)
 if __name__=="__main__":
     parser=argparse.ArgumentParser("Vardict call CNV.")
-    parser.add_argument("-r","--reads",help="The minimum # of variant reads, default 5",required=True,default=5,type=int)
+    parser.add_argument("-m","--min",help="The minimum # of variant reads, default 5",required=True,default=5,type=int)
     parser.add_argument("-tb","--tb",help="tumor bam file",required=True)
     parser.add_argument("-nb","--nb",help="normal bam file",required=True)
     parser.add_argument("-v","--vaf",help="The threshold for allele frequenc",choices=[0.01,0.001,0.02,0.05,0.005],required=True,type=float)
