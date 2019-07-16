bc2727a8307b7c53b26635816ca2bcfa765f4bdc
fanyucai1 <fanyucai1@126.com>
2019-07-16 12:20:32 +0800

    no

--- vardict/vardict_pair.py
+++ vardict/vardict_pair.py
@@ -1,23 +1,27 @@
 import subprocess
-import argparse
-env="export PATH=/software/java/jdk1.8.0_202/bin:/software/R/R-v3.5.2/bin/:"
-env+="/software/vardict/1.5.7/VarDictJava-1.5.7/bin:/software/perl/perl-v5.28.1/bin/:$PATH"
-
-def tumor_normal(args):
+import sys
+software="export PATH=/software/java/jdk1.8.0_202/bin:/software/R/R-v3.5.2/bin/:"
+software+="/software/vardict/1.5.7/VarDictJava-1.5.7/bin:/software/perl/perl-v5.28.1/bin/:$PATH"
+hg19="/data/Database/hg19/ucsc.hg19.fasta"
+def tumor_normal(vaf,tumor_name,min_reads,tumor_bam,normal_bam,bed,normal_name,outdir,ref=hg19,env=software):
     cmd="%s && VarDict -q 20 -Q 20 -G %s -f %s -N %s -r %s -b \"%s|%s\" -z -c 1 -S 2 -E 3 -g 4 %s |testsomatic.R |var2vcf_paired.pl -M -N \"%s|%s\" -f %s >%s/%s.vardict.vcf" \
-        %(env,args.ref,args.vaf,args.tn,args.min,args.tb,args.nb,args.bed,args.tm,args.nn,args.vaf,args.outdir,args.tn)
+        %(env,ref,vaf,tumor_name,min_reads,tumor_bam,normal_bam,bed,tumor_name,normal_name,vaf,outdir,tumor_name)
     subprocess.check_call(cmd,shell=True)
 if __name__=="__main__":
-    parser=argparse.ArgumentParser("Vardict call CNV.")
-    parser.add_argument("-m","--min",help="The minimum # of variant reads, default 5",required=True,default=5,type=int)
-    parser.add_argument("-tb","--tb",help="tumor bam file",required=True)
-    parser.add_argument("-nb","--nb",help="normal bam file",required=True)
-    parser.add_argument("-v","--vaf",help="The threshold for allele frequenc",choices=[0.01,0.001,0.02,0.05,0.005],required=True,type=float)
-    parser.add_argument("-tn","--tn",required=True,help="tumor sample name")
-    parser.add_argument("-nn", "--nn", required=True, help="normal sample name")
-    parser.add_argument("-o","--outdir",required=True,help="output directory")
-    parser.add_argument("-b","--bed",required=True,help="bed file")
-    parser.add_argument("-r","--ref",help="reference fasta",default="/data/Database/hg19/ucsc.hg19.fasta")
-    parser.set_defaults(func=tumor_normal)
-    args=parser.parse_args()
-    args.func()
+    if len(sys.argv<9):
+        print("perl %s tumor_bam normal_bam tumor_name normal_name min_reads bed vaf outdir" %(sys.argv[0]))
+        print("\n\nEmail:fanyucai1@126.com")
+        print("2019.7.16")
+    else:
+        tumor_bam=sys.argv[1]
+        normal_bam=sys.argv[2]
+        tumor_name=sys.argv[3]
+        normal_name=sys.argv[4]
+        min_reads=sys.argv[5]
+        bed=sys.argv[6]
+        vaf=sys.argv[7]
+        outdir=sys.argv[8]
+        if len(sys.argv==11):
+            ref=sys.argv[9]
+            env=sys.argv[10]
+        tumor_normal(vaf, tumor_name, min_reads, tumor_bam, normal_bam, bed, normal_name, outdir, ref=hg19,env=software)
