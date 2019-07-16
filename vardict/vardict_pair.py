f8e4dbf21a9f267c3c3b307430107c94f923c156
fanyucai1 <fanyucai1@126.com>
2019-07-16 14:28:10 +0800

    no

--- vardict/vardict_pair.py
+++ vardict/vardict_pair.py
@@ -1,12 +1,29 @@
 import subprocess
 import sys
+import re
 software="export PATH=/software/java/jdk1.8.0_202/bin:/software/R/R-v3.5.2/bin/:"
-software+="/software/vardict/1.5.7/VarDictJava-1.5.7/bin:/software/perl/perl-v5.28.1/bin/:$PATH"
+software+="/software/vardict/VarDict-1.6.0/bin:/software/perl/perl-v5.28.1/bin/:$PATH"
 hg19="/data/Database/hg19/ucsc.hg19.fasta"
 def tumor_normal(vaf,tumor_name,min_reads,tumor_bam,normal_bam,bed,normal_name,outdir,ref=hg19,env=software):
     cmd="%s && VarDict -th 20 -q 20 -Q 20 -G %s -f %s -N %s -r %s -b \"%s|%s\" -z -c 1 -S 2 -E 3 -g 4 %s |testsomatic.R |var2vcf_paired.pl -M -N \"%s|%s\" -f %s >%s/%s.vardict.vcf" \
         %(env,ref,vaf,tumor_name,min_reads,tumor_bam,normal_bam,bed,tumor_name,normal_name,vaf,outdir,tumor_name)
     subprocess.check_call(cmd,shell=True)
+    infile=open("%s/%s.vardict.vcf"%(outdir,tumor_name),"r")
+    outfile=open("%s/%s.somatic.vcf"%(outdir,tumor_name),"w")
+    for line in infile:
+        line=line.strip()
+        if line.startswith("#"):
+            outfile.write("%s\n"%(line))
+        else:
+            p1=re.compile(r'Somatic')
+            p2=re.compile(r'PASS')
+            a=p1.findall(line)
+            b=p2.findall(line)
+            if a!=[] and b!=[]:
+                outfile.write("%s\n" % (line))
+    outfile.close()
+    infile.close()
+
 if __name__=="__main__":
     if len(sys.argv)<9:
         print("perl %s tumor_bam normal_bam tumor_name normal_name min_reads bed vaf outdir" %(sys.argv[0]))
