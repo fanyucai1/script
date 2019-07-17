cf8636972b4b5646ab5a298e0fbd096946db30ee
fanyucai1 <fanyucai1@126.com>
2019-07-17 17:48:53 +0800

    no

--- ABRA.py
+++ ABRA.py
@@ -16,15 +16,14 @@ import argparse
 import subprocess
 import time
 parser=argparse.ArgumentParser("This script will use ABRA to improve coding indel detection via assembly-based realignment")
-parser.add_argument("-n","--nbam",help="normal bam file",required=True)
-parser.add_argument("-t","--tbam",help="tumal bam file",required=True)
+parser.add_argument("-b","--bam",help="normal bam file",required=True)
+parser.add_argument("-p","--prefix",help="prefix of output",required=True)
 parser.add_argument("-o","--out",help="output directory",default=os.getcwd())
 parser.add_argument("-v","--vcf",help="VCF containing known (or suspected) variant sites.",default=0)
 parser.add_argument("-b","--bed",help="target bed region",default=0)
 
 args=parser.parse_args()
-args.nbam=os.path.abspath(args.nbam)
-args.tbam=os.path.abspath(args.tbam)
+args.bam=os.path.abspath(args.bam)
 if not os.path.exists(args.out):
     os.mkdir(args.out)
     args.out=os.path.abspath(args.out)
@@ -33,14 +32,12 @@ if args.bed!=0:
     par+=" --targets %s"%(args.bed)
 if args.vcf !=0:
     par+=" --in-vcf %s "%(args.vcf)
-cmd="cd %s && %s -Xmx20G -jar %s --in %s,%s --out %s.abra.bam,%s.abra.bam --ref %s --threads 20 %s --tmpdir ./ >abra.log" \
-    %(args.out,java,abra,args.nbam,args.tbam,args.nbam,args.tbam,ref,par)
+cmd="cd %s && %s -Xmx20G -jar %s --in %s --out %s/%s.abra.bam --ref %s --threads 10 %s --tmpdir ./ >abra.log" \
+    %(args.out,java,abra,args.bam,args.out,args.prefix,ref,par)
 
 start=time.time()
 subprocess.check_call(cmd,shell=True)
-p1="%s index %s.abra.bam" %(samtools,args.tbam)
-p2="%s index %s.abra.bam" %(samtools,args.nbam)
-subprocess.check_call(p1,shell=True)
-subprocess.check_call(p2,shell=True)
+p="%s index %s/%s.abra.bam" %(samtools,args.out,args.prefix)
+subprocess.check_call(p,shell=True)
 end=time.time()
 print("Elapsed time was %g seconds" % (end - start))
