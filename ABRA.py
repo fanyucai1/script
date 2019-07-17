ef3c54112fcfac1221bc3f802915903b3b65a1aa
fanyucai1 <fanyucai1@126.com>
2019-07-17 17:39:09 +0800

    no

--- ABRA.py
+++ ABRA.py
@@ -1,11 +1,10 @@
 #Email:fanyucai1@126.com
 #2019.1.21
 
-abra="/data/fyc_software/abra2/abra2-2.19/target/abra2-2.19-jar-with-dependencies.jar"
-java="/data/fyc_software/java/jdk1.8.0_201/bin/java"
-ref="/data/fyc_data/hg19/ucsc.hg19.fasta"
-samtools="/data/fyc_software/samtools/samtools-1.9/samtools"
-env="export LD_LIBRARY_PATH=/data/fyc_software/xz/xz-v5.2.2/lib/:$LD_LIBRARY_PATH"
+abra="/software/abra2/abra2-2.19.jar"
+java="/software/java/jdk1.8.0_202/bin/java"
+ref="/data/Database/hg19/ucsc.hg19.fasta"
+samtools="/software/samtools/samtools-1.9/bin/samtools"
 """
 https://github.com/mozack/abra2/releases
 https://impact-pipeline.readthedocs.io/en/latest/index.html
@@ -16,11 +15,11 @@ import os
 import argparse
 import subprocess
 import time
-import multiprocessing
 parser=argparse.ArgumentParser("This script will use ABRA to improve coding indel detection via assembly-based realignment")
 parser.add_argument("-n","--nbam",help="normal bam file",required=True)
 parser.add_argument("-t","--tbam",help="tumal bam file",required=True)
 parser.add_argument("-o","--out",help="output directory",default=os.getcwd())
+parser.add_argument("-v","--vcf",help="VCF containing known (or suspected) variant sites.",default=0)
 parser.add_argument("-b","--bed",help="target bed region")
 
 args=parser.parse_args()
@@ -33,12 +32,15 @@ par=" --kmer 43 "
 if os.path.exists(args.bed):
     par+=" --targets "
     par+=os.path.abspath(args.bed)
-cmd="cd %s && %s -Xmx20G -jar %s --in %s,%s --out %s.abra.bam,%s.abra.bam --ref %s --threads 20 %s --tmpdir ./ >abra.log" %(args.out,java,abra,args.nbam,args.tbam,args.nbam,args.tbam,ref,par)
+if args.vcf !=0:
+    par+=" --in-vcf %s "%(args.vcf)
+cmd="cd %s && %s -Xmx20G -jar %s --in %s,%s --out %s.abra.bam,%s.abra.bam --ref %s --threads 20 %s --tmpdir ./ >abra.log" \
+    %(args.out,java,abra,args.nbam,args.tbam,args.nbam,args.tbam,ref,par)
 
 start=time.time()
 subprocess.check_call(cmd,shell=True)
-p1="%s && %s index %s.abra.bam" %(env,samtools,args.tbam)
-p2="%s && %s index %s.abra.bam" %(env,samtools,args.nbam)
+p1="%s index %s.abra.bam" %(samtools,args.tbam)
+p2="%s index %s.abra.bam" %(samtools,args.nbam)
 subprocess.check_call(p1,shell=True)
 subprocess.check_call(p2,shell=True)
 end=time.time()
