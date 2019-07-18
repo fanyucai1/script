17f2ff08e5ee7941207de9144f02e239111b59a9
fanyucai1 <fanyucai1@126.com>
2019-07-18 11:51:20 +0800

    no

---
+++ copy.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import re
+
+samplelist=sys.argv[1]
+dir=sys.argv[2]
+outdir=sys.argv[3]
+
+infile=open(samplelist,'r')
+sampleID=[]
+for line in infile:
+    line=line.strip()
+    array=line.split("\t")
+    sampleID[array[0]]=1
+cmd="scp "
+for (root,dirs,files) in os.walk(dir):
+    for name in files:
+        file=os.path.join(root, name)
+        p=re.compile(r'SmallVariants.genome.vcf$')
+        a=p.findall(file)
+        if a!=[]:
+            cmd+=" %s "%(a[0])
+cmd+=" fanyucai@192.168.1.118:%s "%(outdir)
+print(cmd)
