859e5b5cfc3d564750d68db9896982d4c7d55de1
fanyucai1 <fanyucai1@126.com>
2019-07-18 13:16:12 +0800

    no

--- copy.py
+++ copy.py
@@ -16,9 +16,11 @@ cmd="scp "
 for (root,dirs,files) in os.walk(dir):
     for name in files:
         file=os.path.join(root, name)
-        p=re.compile(r'SmallVariants.genome.vcf$')
-        a=p.findall(file)
-        if a!=[]:
+        p1=re.compile(r'SmallVariants.genome.vcf$')
+        p2=re.compile(r'BJ19\S+smCounter.cut.vcf')
+        a=p1.findall(file)
+        b=p2.findall(file)
+        if a!=[] or b!=[]:
             cmd+=" %s "%(file)
 cmd+=" fanyucai@192.168.1.118:%s "%(outdir)
 subprocess.check_call('echo %s >run.sh'%(cmd),shell=True)
