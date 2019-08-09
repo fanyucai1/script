e5d6438e2e72339ad735f15cd0f64c9e7119010c
fanyucai1 <fanyucai1@126.com>
2019-08-09 15:03:58 +0800

    no

---
+++ file_copy.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import re
+import subprocess
+def copy(indir,keywords,outdir):
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    for(root,dir,files) in os.walk(indir):
+        for file in files:
+            path=os.path.join(root,file)
+            if re.search(keywords,path):
+                subprocess.call('cp %s %s'%(path,outdir),shell=True)
+
+if __name__ == '__main__':
+    if len(sys.argv)!=4:
+        print("usage:python3 %s inputdir keywords outdir"%(sys.argv[0]))
+        print("Email:fanyucai1@126.com")
+    else:
+        indir=sys.argv[1]
+        keywords=sys.argv[2]
+        outdir=sys.argv[3]
+        run(indir, keywords, outdir)
