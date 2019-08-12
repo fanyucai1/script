0671c028191ae5d17dcf54b52e60283cc122add7
fanyucai1 <fanyucai1@126.com>
2019-08-12 16:38:57 +0800

    no

--- sample_QC.py
+++ sample_QC.py
@@ -20,7 +20,7 @@ def run(target_bed,probe_bed,bam,outdir,prefix):
         " BAIT_INTERVALS=%s/probe.interval_list COVMAX=1000000 "%(picard,bam,out,ref,outdir,outdir)
     subprocess.check_call(cmd,shell=True)
     ####Assess sequence coverage by a wide array of metrics, partitioned by sample, read group, or library#############
-    cmd="java -Xmx40g -jar %s -T DepthOfCoverage -allowPotentiallyMisencodedQuals --minBaseQuality 20 --minMappingQuality 20 -R %s -I %s -o %s -ct 50 -ct 100 -ct 250 -ct 500 -ct 1000 -ct 10000 -L %s/target.interval_list"\
+    cmd="java -Xmx40g -jar %s -T DepthOfCoverage -allowPotentiallyMisencodedQuals --minBaseQuality 20 --minMappingQuality 20 -R %s -I %s -o %s -ct 1 -ct 5 -ct 10 -ct 50 -ct 100 -ct 250 -ct 500 -ct 1000 -ct 10000 -L %s/target.interval_list"\
         %(gatk3,ref,bam,out,outdir)
     subprocess.check_call(cmd,shell=True)
     ################Count On-Target Reads##########
@@ -79,6 +79,12 @@ def run(target_bed,probe_bed,bam,outdir,prefix):
                 name.append(array[i])
         if num==2:
             for i in range(len(array)):
+                if name[i]=="%_bases_above_1":
+                    outfile.write("%%_bases_above_1\t%s\n" % (array[i]))
+                if name[i]=="%_bases_above_5":
+                    outfile.write("%%_bases_above_5\t%s\n" % (array[i]))
+                if name[i]=="%_bases_above_10":
+                    outfile.write("%%_bases_above_10\t%s\n" % (array[i]))
                 if name[i]=="%_bases_above_50":
                     outfile.write("%%_bases_above_50\t%s\n" % (array[i]))
                 if name[i] == "%_bases_above_100":
