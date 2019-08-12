05316025312cd02d2b23c3b6a082cb96c1a5ad83
fanyucai1 <fanyucai1@126.com>
2019-08-12 14:42:16 +0800

    no

--- sample_QC.py
+++ sample_QC.py
@@ -4,10 +4,14 @@ import subprocess
 picard="/software/picard/picard.jar"
 ref="/data/Database/hg19/ucsc.hg19.fasta"
 gatk3="/software/gatk/3.7/GenomeAnalysisTK.jar"
-def run(target_bed,probe_bed,bam,outdir,prefix):
+bedtools="/software/bedtools/bedtools2/bin/bedtools"
+def run(target_bed,probe_bed,bam,depth,outdir,prefix):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     out=outdir+"/"+prefix
+    dict = {}
+    outputname=[]
+    outfile = open("%s.sample.qc.tsv", "w")
     ####BedToIntervalList (Picard)#############
     cmd="java -jar %s BedToIntervalList I=%s O=%s/target.interval_list SD=%s" %(picard,target_bed,outdir,bam)
     subprocess.check_call(cmd,shell=True)
@@ -18,15 +22,21 @@ def run(target_bed,probe_bed,bam,outdir,prefix):
         " BAIT_INTERVALS=%s/probe.interval_list COVMAX=1000000 "%(picard,bam,out,ref,outdir,outdir)
     subprocess.check_call(cmd,shell=True)
     ####Assess sequence coverage by a wide array of metrics, partitioned by sample, read group, or library#############
-    cmd="java -Xmx40g -jar %s -T DepthOfCoverage -allowPotentiallyMisencodedQuals --minBaseQuality 20 --minMappingQuality 20 -R %s -I %s -o %s -ct 50 -L %s/target.interval_list"\
+    cmd="java -Xmx40g -jar %s -T DepthOfCoverage -allowPotentiallyMisencodedQuals --minBaseQuality 20 --minMappingQuality 20 -R %s -I %s -o %s -ct 50 -ct 100 -ct 250 -ct 500 -ct 1000 -ct 10000 -L %s/target.interval_list"\
         %(gatk3,ref,bam,out,outdir)
     subprocess.check_call(cmd,shell=True)
-    ############################################
+    ################Count On-Target Reads##########
+    cmd = os.popen("%s intersect -bed -u -abam %s -b %s | wc -l"%(bedtools,bam,target_bed))
+    dict['On-Target_Reads'] = cmd.read()
+    dict['On-Target_Reads'] = dict.strip()
+    outputname.append("counts-On-Target_Reads")
+    ################InsertSize####################
+    cmd="java -Xmx40g -jar %s CollectInsertSizeMetrics I=%s O=%s.insert_size_metrics.txt H=%s.insert_size_histogram.pdf"%(picard,bam,out,out)
+    subprocess.check_call(cmd,shell=True)
+    ##############################################
     infile=open("%s.hs_metrics.txt"%(out),"r")
-    outfile=open("%s.sample.qc.tsv","w")
     num=0
     name=[]
-    dict={}
     for line in infile:
         if not line.startswith("#"):
             array=line.split("\t")
@@ -36,14 +46,81 @@ def run(target_bed,probe_bed,bam,outdir,prefix):
                 num+=1
             if num==1 and len(array)==len(name):
                 for i in range(len(array)):
-                    if name[i]=="PCT_TARGET_BASES_100X":
-                        dict['PCT_TARGET_BASES_100X']=array[i]
-                    if name[i] == "PCT_TARGET_BASES_100X":
+                    if name[i] == "FOLD_80_BASE_PENALTY":
+                        dict['FOLD_80_BASE_PENALTY'] = array[i]
+                        outputname.append("FOLD_80_BASE_PENALTY")
+                    if name[i] == "MEAN_TARGET_COVERAGE":
+                        dict['MEAN_TARGET_COVERAGE'] = array[i]
+                        outputname.append("mean_depth")
+                    if name[i] == "MEDIAN_TARGET_COVERAGE":
+                        dict['MEDIAN_TARGET_COVERAGE'] = array[i]
+                        outputname.append("median_depth")
+    infile.close()
+    infile=open("%s.insert_size_metrics.txt"%(out),"r")
+    num=0
+    name=[]
+    for line in infile:
+        if not line.startswith("#"):
+            line = line.strip()
+            array = line.split("\t")
+            if array[0]=="MEDIAN_INSERT_SIZE":
+                for i in range(len(array)):
+                    name.append(array[i])
+                num+=1
+            if num==1 and len(array)==len(name):
+                dict['MEDIAN_INSERT_SIZE'] = array[0]
+                outputname.append("insert_size")
+    infile.close()
+    infile = open("%s.sample_summary" % (out), "r")
+    num = 0
+    name = []
+    for line in infile:
+        num+=1
+        line = line.strip()
+        array = line.split("\t")
+        if num==1:
+            for i in range(len(array)):
+                name.append(array[i])
+        if num==2:
+            for i in range(len(array)):
+                if name[i]=="%_bases_above_50":
+                    dict["%_bases_above_50"]=array[i]
+                    outputname.append("%_bases_above_50")
+                if name[i] == "%_bases_above_100":
+                    dict["%_bases_above_100"] = array[i]
+                    outputname.append("%_bases_above_100")
+                if name[i]=="%_bases_above_250":
+                    dict["%_bases_above_250"] = array[i]
+                    outputname.append("%_bases_above_250")
+                if name[i] == "%_bases_above_500":
+                    dict["%_bases_above_500"] = array[i]
+                    outputname.append("%_bases_above_500")
+                if name[i] == "%_bases_above_1000":
+                    dict["%_bases_above_1000"] = array[i]
+                    outputname.append("%_bases_above_1000")
+                if name[i] == "%_bases_above_10000":
+                    dict["%_bases_above_10000"] = array[i]
+                    outputname.append("%_bases_above_10000")
+    infile.close()
+    for i in range(len(outputname)):
+        if i==0:
+            outfile.write("%s"%(outputname[i]))
+        else:
+            outfile.write("\t%s" % (outputname[i]))
+    outfile.write("\n")
+    for i in range(len(outputname)):
+        if i == 0:
+            outfile.write("%s" % (dict[outputname[i]]))
+        else:
+            outfile.write("\t%s" % (dict[outputname[i]]))
+    outfile.write("\n")
+    outfile.close()
 
 
 if __name__=="__main__":
     if (len(sys.argv)!=6):
         print("\nUsage:python3 %s target_bed probe_bed bamfile outdir prefix"%(sys.argv[0]))
+        print("target_bed and probe_be maybe a same file")
         print("\nEmail:fanyucai1@126.com")
     else:
         target_bed=sys.argv[1]
