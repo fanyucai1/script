a584ad5cc374aa93e769720fc885c398ca196fcc
fanyucai1 <fanyucai1@126.com>
2019-08-29 14:23:22 +0800

    no

--- pre_TSO500.py
+++ pre_TSO500.py
@@ -1,21 +1,26 @@
 import os
 import sys
 import subprocess
-import re
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 from multiprocessing import Process
 fastp="/software/fastp/fastp"
+TSO500="/software/TSO500/1.3.1/TruSight_Oncology_500.sh"
+resources="/software/TSO500/1.3.1/resources"
+samplesheet="/data/TSO500/SampleSheet.csv"
+samplelist="/data/TSO500/samplelist.csv"
 
 def shell_run(x):
     subprocess.check_call(x, shell=True)
 def run(pe1,pe2,index2,outdir,prefix):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
-    out=outdir+"/"+prefix
-    cmd1="%s -i %s -I %s -U --umi_loc per_read --umi_len 7 --umi_skip 1 -o %s.umi.1.fq.gz -O %s.umi.2.fq.gz"\
+    if not os.path.exists("%s/%s"%(outdir,prefix)):
+        os.mkdir("%s/%s"%(outdir,prefix))
+    out=outdir+"/"+prefix+"/"+prefix
+    cmd="%s -i %s -I %s -U --umi_loc per_read --umi_len 7 --umi_skip 1 -o %s.umi.1.fq.gz -O %s.umi.2.fq.gz"\
         %(fastp,pe1,pe2,out,out)
-    subprocess.check_call(cmd1,shell=True)
+    subprocess.check_call(cmd,shell=True)
     my_seq = Seq('%s'%(index2), IUPAC.unambiguous_dna)
     string = {}
     string["a"]="zcat < %s.umi.1.fq.gz|sed s:%s:%s:g|sed s:_:+:g|gzip -c >%s_S1_L001_R1_001.fastq.gz && rm %s.umi.1.fq.gz" \
@@ -28,6 +33,7 @@ def run(pe1,pe2,index2,outdir,prefix):
     p2.start()
     p1.join()
     p2.join()
+
 
 if __name__=="__main__":
     if len(sys.argv)!=6:
