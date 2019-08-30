250bce111a052784a527ed6d9b91e47d624461f1
fanyucai1 <fanyucai1@126.com>
2019-08-30 11:42:47 +0800

    no

--- TSO500/core/filter_normal.py
+++ TSO500/core/filter_normal.py
@@ -35,7 +35,7 @@ for (root,dirs,files) in os.walk(root_dir):
                         if depth >50:
                             if float(array[-1].strip("%s"))<=40:
                                 var_num+=1
-                            if float(array[-1].strip("%s")) > 50 and float(array[-1].strip("%s")) < 95:
+                            if float(array[-1].strip("%s")) > 60 and float(array[-1].strip("%s")) < 95:
                                 var_num += 1
                 infile.close()
                 outfile.write("%s\t%s\t%s\n"%(b[0],num,var_num))
