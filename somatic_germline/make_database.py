ca59e74ad8e24fbc83b8301b49a4bd9a58cac10f
fanyucai1 <fanyucai1@126.com>
2019-09-03 13:05:06 +0800

    no

---
+++ somatic_germline/make_database.py
@@ -0,0 +1,82 @@
+import os
+import re
+import argparse
+
+cosmic_anno="/data/Database/COSMIC/release_v88/CosmicMutantExport.tsv"
+cosmic_vcf="/data/Database/COSMIC/release_v88/CosmicCodingMuts.vcf"
+clinvar_vcf="/data/Database/clinvar/clinvar.vcf"
+clinvar_anno="/data/Database/clinvar/variant_summary.txt"
+
+def run(outdir,prefix):
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    out=outdir+"/"+prefix
+    outfile1=open("%s.somatic.vcf"%(out),"w")
+    outfile2 = open("%s.germline.vcf"%(out), "w")
+    outfile3 = open("%s.snp.vcf"%(out), "w")
+    outfile1.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
+    outfile2.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
+    outfile3.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
+    dict=[]
+    ############################cosmic
+    infile=open(cosmic_vcf,"r")
+    id_num,status,num,cosmicID= 0,0,0,[]
+    for line in infile:
+        num += 1
+        line = line.strip()
+        array = line.split("\t")
+        if num == 1:
+            for i in range(len(array)):
+                if array[i] == "Mutation ID":
+                    id_num = i
+                if array[i] == "Mutation somatic status":
+                    status = i
+        else:
+            if array[status]=="Confirmed somatic variant":
+                cosmicID.append(id_num)
+    infile.close()
+    infile=open(cosmic_vcf,"r")
+    for line in infile:
+        line = line.strip()
+        array = line.split("\t")
+        if not line.startswith("#"):
+            tmp=array[0]+"\t"+array[1]+"\t"+array[3]+"\t"+array[4]
+            if re.search(r'SNP',line):
+                outfile3.write("%s\n"%(line))
+            elif array[2] in cosmicID:
+                outfile1.write("%s\n"%(line))
+                dict[tmp]=1
+            else:
+                pass
+    infile.close()
+    ############################clinvar
+    status,AlleleID_somatic,AlleleID_germline=0,[],[]
+    infile=open(clinvar_anno,"r")
+    p=re.compile(r'GRCh37')
+    for line in infile:
+        line = line.strip()
+        array = line.split("\t")
+        if line.startswith("#"):
+            for i in range(len(array)):
+                if array[i] == "OriginSimple":
+                    status = i
+        else:
+            if re.search(r'GRCh37',line) and re.search(r'somatic',array[status]):
+                AlleleID_somatic.append(array[0])
+            else:
+                if re.search(r'GRCh37', line) and re.search(r'germline', array[status]):
+                    AlleleID_germline.append(array[0])
+    infile.close()
+    infile=open(clinvar_vcf,"r")
+    p=re.compile(r'ALLELEID=(\d+)')
+    for line in infile:
+        line = line.strip()
+        array = line.split("\t")
+        if not line.startswith("#"):
+            tmp = array[0] + "\t" + array[1] + "\t" + array[3] + "\t" + array[4]
+            if not tmp in dict:
+                a=p.findall(line)
+                if a[0] in AlleleID_somatic:
+                    outfile1.write("%s\n"%(line))
+                if a[0] in AlleleID_germline:
+                    outfile2.write("%s\n" % (line))
