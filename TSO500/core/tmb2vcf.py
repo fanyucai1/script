55f9083a7047cd170bd43c9b4023ace6d945c60b
fanyucai1 <fanyucai1@126.com>
2019-09-04 13:39:40 +0800

    no

--- TSO500/core/tmb2vcf.py
+++ TSO500/core/tmb2vcf.py
@@ -11,16 +11,12 @@ def run(root_dir,outdir):
     ######################################get SNV information
     for (root,dirs,files) in os.walk(root_dir):
         for file in files:
-            tmp=os.path.join(root,file)
-            array=tmp.split("/")
-            samplename=re.sub(r'_SmallVariants.genome.vcf',"",array[-1])
-            if samplename in sample_ID:
-                dict={}
-                outfile = open("%s/%s.snv.tmp.vcf" % (outdir, samplename), "w")
-                if tmp.endswith("tmb.tsv"):
-                    path=tmp
+            path=os.path.join(root,file)
+            array=path.split("/")
+            if path.endswith("tmb.tsv"):
+                samplename=array[-2]
+                if samplename in sample_ID:
                     infile = open(path, "r")
-                    outfile.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
                     num = 0
                     dict = {}
                     for line in infile:
@@ -30,18 +26,22 @@ def run(root_dir,outdir):
                         tmp = array[0] + "\t" + array[1] + "\t" + array[2] + "\t" + array[3]
                         dict[tmp] = 1
                     infile.close()
-                if tmp.endswith("SmallVariants.genome.vcf"):
-                    vcf=tmp
-                    infile=open(vcf,"r")
-                    for line in infile:
-                        line=line.strip()
-                        if not line.startswith("#"):
-                            array=line.split("\t")
-                            tmp=array[0] + "\t" + array[1] + "\t" + array[3] + "\t" + array[4]
-                            if tmp in dict:
-                                info=array[-1].split(":")
-                                outfile.write("%s\t%s\t.\t%s\t%s\t.\t.\tGT=%s;AD=%s;Var=%s\n"%(array[0],array[1],array[3],array[4],info[0],info[2],info[4]))
-                    infile.close()
-                outfile.close()
+                    outfile = open("%s/%s.snv.tmp.vcf" % (outdir, samplename), "w")
+                    outfile.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
+                    for (root, dirs, files) in os.walk(root_dir):
+                        for file in files:
+                            vcf = os.path.join(root, file)
+                            if vcf.endswith("%s_SmallVariants.genome.vcf"%(samplename)):
+                                infile=open(vcf,"r")
+                                for line in infile:
+                                    line=line.strip()
+                                    if not line.startswith("#"):
+                                        array=line.split("\t")
+                                        tmp=array[0] + "\t" + array[1] + "\t" + array[3] + "\t" + array[4]
+                                        if tmp in dict:
+                                            info=array[-1].split(":")
+                                            outfile.write("%s\t%s\t.\t%s\t%s\t.\t.\tGT=%s;AD=%s;Var=%s\n"%(array[0],array[1],array[3],array[4],info[0],info[2],info[4]))
+                                infile.close()
+                    outfile.close()
 if __name__=="__main__":
     run(root_dir,outdir)
