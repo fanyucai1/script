a82c2e34b01009764db8d4b6393dd3e7e1f55cda
fanyucai1 <fanyucai1@126.com>
2019-09-04 13:44:53 +0800

    no

--- TSO500/core/tmb2vcf.py
+++ TSO500/core/tmb2vcf.py
@@ -1,6 +1,4 @@
 import os
-import sys
-import re
 root_dir="/data/TSO500"
 outdir="/data/TSO500/snv_vcf"
 def run(root_dir,outdir):
@@ -19,12 +17,25 @@ def run(root_dir,outdir):
                     infile = open(path, "r")
                     num = 0
                     dict = {}
+                    f1, f2, f3, f4= 0, 0, 0, 0
                     for line in infile:
                         line = line.strip()
-                        num += 1
                         array = line.split("\t")
-                        tmp = array[0] + "\t" + array[1] + "\t" + array[2] + "\t" + array[3]
-                        dict[tmp] = 1
+                        num += 1
+                        if num==1:
+                            for k in range(len(array)):
+                                if array[k] == "GermlineFilterDatabase":
+                                    f1 = k
+                                if array[k] == "SomaticStatus":
+                                    f2 = k
+                                if array[k] == "CodingVariant":
+                                    f3 = k
+                                if array[k] == "GermlineFilterProxi":
+                                    f4 = k
+                        else:
+                            if array[f1] == "False" and array[f2] == "Somatic" and array[f3] == "True" and array[f4] == "False":
+                                tmp = array[0] + "\t" + array[1] + "\t" + array[2] + "\t" + array[3]
+                                dict[tmp] = 1
                     infile.close()
                     outfile = open("%s/%s.snv.tmp.vcf" % (outdir, samplename), "w")
                     outfile.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
