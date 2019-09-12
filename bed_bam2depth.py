60c38c79515f23d0307922486df1d193e89c52ae
fanyucai1 <fanyucai1@126.com>
2019-09-12 14:46:38 +0800

    no

--- bed_bam2depth.py
+++ bed_bam2depth.py
@@ -14,22 +14,45 @@ def run(bed,bam,outdir,prefix):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     out=outdir+"/"+prefix
-    cmd="%s coverage -a %s -b %s -mean >%s.MeanCoverageBED.bedgraph"%(bedtools,bed,bam,out)
+    cmd="%s coverage -a %s -b %s -d >%s.site.depth"%(bedtools,bed,bam,out)
     subprocess.check_call(cmd,shell=True)
-    infile=open("%s.MeanCoverageBED.bedgraph"%(out),"r")
-    x,y=[],[]
+    infile=open("%s.site.depth"%(out),"r")
     num=0
+    x0,x50,x100,x250,x500,x1000,x3500=0,0,0,0,0,0,0
     for line in infile:
         num+=1
-        x.append(num)
         line=line.strip()
         array=line.split("\t")
-        y.append(float(array[-1]))
+        if float(array[-1]) < 50:
+            x0+=1
+        if float(array[-1]) >=50:
+            x50+=1
+        if float(array[-1]) >= 100:
+            x100+=1
+        if float(array[-1]) >=250:
+            x250+=1
+        if float(array[-1]) >= 500:
+            x500+=1
+        if float(array[-1]) >= 1000:
+            x1000+=1
+
+    x0 = x0 / num * 100
+    x50=x50/num*100
+    x100 = x100 / num * 100
+    x250 = x250 / num * 100
+    x500 = x500 / num * 100
+    x1000 = x1000 / num * 100
+    y=[x0,x50,x100,x250,x500,x1000]
+    x=["0",'>=50X','>=100X','>=250X','>=500X','>=1000X']
     plt.figure(figsize=(18, 10))
-    plt.xlabel("%s Count_bins"%(num))
-    plt.ylabel("Mean_Depth")
+    plt.xlabel("Depth(X)")
+    plt.ylabel("Coverage")
     sns.lineplot(x=x, y=sorted(y))
     plt.savefig('%s/bed_depth.png' % (outdir), dpi=300)
+    cmd="%s coverage -a %s -b %s -hist >%s.beddepth"%(bedtools,bed,bam,out)
+    subprocess.check_call(cmd,shell=True)
+
+
 
 
 
