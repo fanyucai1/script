5f75deac0861ed14766d003ecf8b2138c6ad3f03
fanyucai1 <fanyucai1@126.com>
2019-09-19 16:42:00 +0800

    no

--- TSO500/core/pre_TSO500_v2.0.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import subprocess
-from Bio.Seq import Seq
-from Bio.Alphabet import IUPAC
-from multiprocessing import Process
-fastp="/software/fastp/fastp"
-TSO500="/software/TSO500/1.3.1/TruSight_Oncology_500.sh"
-
