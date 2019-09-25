c90c2df19fe77b032c4a537b0c7476e191b71e74
fanyucai1 <fanyucai1@126.com>
2019-09-25 16:52:30 +0800

    no

--- TSO500/core/UMI_stat.py
+++ TSO500/core/UMI_stat.py
@@ -14,23 +14,30 @@ if fastq.endswith("gz"):
 else:
     subprocess.check_call('grep @ %s >%s.tmp.seq'%(fastq,out),shell=True)
 infile=open("%s.tmp.seq"%(out),"r")
-dict={}
+dict1={}
+dict2={}
 for line in infile:
     line=line.strip()
     array=line.split("\t")
     array1=array[0].split(":")
     UMI=array1[-1].split("+")
-    if not UMI[0] in dict:
-        dict[UMI[0]]=0
-    if not UMI[1] in dict:
-        dict[UMI[1]]=0
-    dict[UMI[0]]+=1
-    dict[UMI[1]]+=1
-
+    if not UMI[0] in dict1:
+        dict1[UMI[0]]=0
+    if not UMI[1] in dict2:
+        dict2[UMI[1]]=0
+    dict1[UMI[0]]+=1
+    dict2[UMI[1]]+=1
 infile.close()
-b = sorted(dict.items(), key=operator.itemgetter(1),reverse=True)
 
-outfile=open("%s.UMI.stat"%(out),"w")
+a=sorted(dict1.items(), key=operator.itemgetter(1),reverse=True)
+b=sorted(dict2.items(), key=operator.itemgetter(1),reverse=True)
+
+outfile=open("%s.UMI.1.stat"%(out),"w")
+for key in a:
+    outfile.write("%s,%s"%(key))
+outfile.close()
+
+outfile=open("%s.UMI.2.stat"%(out),"w")
 for key in b:
-    outfile.write("%s"%(key))
+    outfile.write("%s,%s"%(key))
 outfile.close()
