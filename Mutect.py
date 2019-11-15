4b2bcb93f0b5afab120cbc1ce5dea0acfd962ccb
fanyucai1 <fanyucai1@126.com>
2019-11-15 16:14:25 +0800

    no

--- Mutect.py
+++ Mutect.py
@@ -1,5 +1,6 @@
+#Email:fanyucai1@126.com
+
 import os
-import sys
 import configparser
 import argparse
 import subprocess
@@ -19,7 +20,7 @@ def run(tumor_bam,tumor_name,normal_bam,bed,outdir,configfile,pon):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     out=outdir+"/"+tumor_name
-    par=" -unique 2 --callable-depth 30 --create-output-bam-index true --min-base-quality-score 20 --native-pair-hmm-threads 8 "
+    par=" -mnp-dist 2 -unique 5 --callable-depth 30 --create-output-bam-index true --min-base-quality-score 20 --native-pair-hmm-threads 8 "
     par+=" -R %s -bamout %s.bam --germline-resource %s "%(hg19_ref,out,germline_resource)
     if pon!="0":
         par+=" -pon %s "%(pon)
@@ -46,3 +47,12 @@ if __name__=="__main__":
     parser.add_argument("--pon", help="panel-of-normals vcf file",default="0")
     args=parser.parse_args()
     run(args.tbam, args.tname,args.nbam, args.bed, args.outdir, args.configfile,args.pon)
+"""
+1：by fewer than 5 variant reads are typically considered to be likely false positive calls
+2：Any base call with Q<20 should be considered low quality
+3：Any variant that is present on >1% of normal human chromosomes is almost certainly not clinically relevant for cancer. 
+4：variants with VAF at nearly 50% or 100% should be considered potentially germline during analysis
+5：If the VAF is far higher than expected, it could indicate that the variant is either germline or in a region of loss of heterozygosity (LoH).
+Strom S P. Current practices and guidelines for clinical next-generation sequencing oncology testing[J]. Cancer biology & medicine, 2016, 13(1): 3.
+
+"""
