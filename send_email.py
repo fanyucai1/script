70402f1954b4d2ea3fd826556b2d038437f1b6d7
fanyucai1 <fanyucai1@126.com>
2019-12-12 17:14:41 +0800

    no

--- send_email.py
+++ send_email.py
@@ -1,17 +1,13 @@
-#!/usr/bin/python3
-
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 import sys
 
-mail_host="smtp.exmail.qq.com"
-
 def run(prefix,dir):
+    mail_host = "smtp.exmail.qq.com"
     message = MIMEText('%s项目已经运行完成,共享目录是%s'%(prefix,dir), 'plain', 'utf-8')
-    message['From'] = Header("生物信息组", 'utf-8')  # 发送者
-    message['To'] = Header("遗传咨询组", 'utf-8')  # 接收者
-
+    message['From'] = Header("生物信息组", 'utf-8')  #发送者
+    message['To'] = Header("遗传咨询组", 'utf-8')  #接收者
     ##########################邮件主题######################
     subject = 'panel27项目%s分析结果'%(prefix)
     message['Subject'] = Header(subject, 'utf-8')
@@ -25,6 +21,7 @@ def run(prefix,dir):
         print("邮件发送成功")
     except smtplib.SMTPException:
         print("Error: 无法发送邮件")
+
 if __name__=="__main__":
     prefix=sys.argv[1]###项目名称
     dir=sys.argv[2]####共享目录
